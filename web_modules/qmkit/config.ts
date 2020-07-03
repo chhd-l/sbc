1196f340c4e5c9b08a5e6c9099cb4367ac87ac6e
唐井儿儿儿 <mytang11@163.com>
2020-07-03 23:36:43 +0800

    接口地址配置修改

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,6 +1,6 @@
 import Common from './config-common';
 export default {
   // dev
-  HOST: process.env.NODE_ENV === 'development' ? 'http://121.37.129.70:8390' : 'http://52.168.31.130:8390',
+  HOST: process.env.NODE_ENV != 'production' ? 'http://121.37.129.70:8390' : 'http://52.168.31.130:8390',
   ...Common
 };

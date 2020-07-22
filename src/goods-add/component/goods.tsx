f5371bcb896b6d55ce3d7f3f2360edc28684d296
yuqi zhu <yuqizhu@yuqideMacBook-Pro.local>
2020-07-22 16:08:05 +0800

    修改bug

--- src/goods-add/component/goods.tsx
+++ src/goods-add/component/goods.tsx
@@ -295,6 +295,8 @@ class GoodsForm extends React.Component<any, any> {
               )}
             </FormItem>
           </Col>
+        </Row>
+        <Row type="flex" justify="start">
           <Col span={8}>
             <FormItem
               {...formItemLayout}
@@ -326,6 +328,39 @@ class GoodsForm extends React.Component<any, any> {
                 onChange: this._editGoods.bind(this, 'goodsNo'),
                 initialValue: goods.get('goodsNo')
               })(<Input />)}
+            </FormItem>
+          </Col>
+          <Col span={8}>
+            <FormItem
+              {...formItemLayout}
+              label={<FormattedMessage id="product.InternalSPU" />}
+            >
+              {getFieldDecorator('goodsNo', {
+                rules: [
+                  {
+                    required: true,
+                    whitespace: true,
+                    message: 'Please fill in the SPU code'
+                  },
+                  {
+                    min: 1,
+                    max: 20,
+                    message: '1-20 characters'
+                  },
+                  {
+                    validator: (rule, value, callback) => {
+                      QMMethod.validatorEmoji(
+                        rule,
+                        value,
+                        callback,
+                        'SPU encoding'
+                      );
+                    }
+                  }
+                ],
+                onChange: this._editGoods.bind(this, 'goodsNo'),
+                initialValue: goods.get('internalGoodsNo')
+              })(<Input disabled />)}
             </FormItem>
           </Col>
         </Row>
@@ -720,7 +755,21 @@ class GoodsForm extends React.Component<any, any> {
           <Col span={8}>
             <FormItem
               {...formItemLayout}
-              label={<FormattedMessage id="product.productImage" />}
+              label={
+                <span>
+                  <span
+                    style={{
+                      color: 'red',
+                      fontFamily: 'SimSun',
+                      marginRight: '4px',
+                      fontSize: '12px'
+                    }}
+                  >
+                    *
+                  </span>
+                  <FormattedMessage id="product.productImage" />
+                </span>
+              }
             >
               <div style={{ width: 550 }}>
                 <ImageLibraryUpload

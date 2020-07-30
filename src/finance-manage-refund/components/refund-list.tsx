72a9a8e6ea6082eccb66bdaadf3b930e984d6320
drtan <1@1.com>
2020-07-30 11:01:05 +0800

    20200729a

--- src/finance-manage-refund/components/refund-list.tsx
+++ src/finance-manage-refund/components/refund-list.tsx
@@ -89,7 +89,6 @@ export default class RefundList extends React.Component<any, any> {
             key="tradeNo"
           />*/}
 
-          <Column
             title="Customer name"
             dataIndex="customerName"
             key="customerName"
@@ -118,7 +117,8 @@ export default class RefundList extends React.Component<any, any> {
             key="paymentOSActualReturnPrice"
             render={(text, rowData: any) => {
               return text ? <span>{text}</span> : '--';
-            }}
+            }}          <Column
+
           />
         </DataGrid>
       </div>

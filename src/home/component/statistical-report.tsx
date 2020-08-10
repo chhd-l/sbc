15f60f4ece3cf0f5dd0d9f7dc2c723c89fa7c37f
drtan <1@1.com>
2020-08-10 14:23:32 +0800

    20200807

--- src/home/component/statistical-report.tsx
+++ src/home/component/statistical-report.tsx
@@ -10,6 +10,7 @@ import { IList } from 'typings/globalType';
 import FlowTrendsCharts from './flow-trends';
 import CustomerGrowTrendsCharts from './cus-trends';
 import { FormattedMessage } from 'react-intl';
+const visitorsImg = require('/public/images/other/home-icon/btn-visitors.png');
 
 const trafficColumns = [
   {
@@ -166,22 +167,30 @@ export default class StatisticalReport extends React.Component<any, any> {
             <h3>
               <FormattedMessage id="visitsToday" />
             </h3>
-            <div className="dateBg">
-              <div className="dataItem">
-                <label>
-                  <FormattedMessage id="uv" />
-                </label>
-                <strong>{trafficNum.get('totalUv') || 0}</strong>
+            <div className="align-items-center">
+              <div style={{ width: '20%' }}>
+                <img src={visitorsImg} alt="" width={47} height={47} />
+              </div>
+              <div style={{ width: '80%' }}>
+                <div className="dateBg">
+                  <div className="dataItem">
+                    <label>
+                      <FormattedMessage id="uv" />
+                    </label>
+                    <strong>{trafficNum.get('totalUv') || 0}</strong>
+                  </div>
+                </div>
+                <div className="dateBg">
+                  <div className="dataItem">
+                    <label>
+                      <FormattedMessage id="pv" />
+                    </label>
+                    <strong>{trafficNum.get('totalPv') || 0}</strong>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="dateBg">
-              <div className="dataItem">
-                <label>
-                  <FormattedMessage id="pv" />
-                </label>
-                <strong>{trafficNum.get('totalPv') || 0}</strong>
-              </div>
-            </div>
+
             {/* <div className="dateBg">
                 <div className="dataItem">
                   <label>Product visitor number</label>
@@ -221,7 +230,7 @@ export default class StatisticalReport extends React.Component<any, any> {
           <div className="dateBg">
             <div className="dataItem">
               <label>
-                <FormattedMessage id="numberOfPaymentOrders" />
+                <FormattedMessage id="Payment orders" />
               </label>
               <strong>
                 {tradeNum && (tradeNum.get('payOrderCount') || 0)}

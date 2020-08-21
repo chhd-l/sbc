5cd6c4e0d4e9af67ec4cd8212301c97429cff1a2
YangSheng <kensyang@deloitte.com.cn>
2020-08-21 15:22:52 +0800

    okta配置暂存

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,7 +1,9 @@
 import Common from './config-common';
 export default {
   // dev
-  HOST: 'http://121.37.129.70:8390',
+  // HOST: 'http://121.37.129.70:8390',
+  HOST: 'http://124.71.151.9:8390',
+  
   // HOST: 'http://192.168.2.12:8087',
   // HOST: 'http://52.168.31.130:8390',
   // HOST: 'https://storestg.royalcanin.com/api',

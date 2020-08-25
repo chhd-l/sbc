bc6a3b288b9ce6a515d4a2fad3de15ad2d595bf5
YangSheng <kensyang@deloitte.com.cn>
2020-08-25 15:16:16 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
-import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+// import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
 import { Provider } from 'react-redux';
 import { routeWithSubRoutes, history, noop } from 'qmkit';
 import { homeRoutes } from './router';
@@ -32,11 +32,11 @@ const B2BBoss = () => (
         <Router history={history}>
           <div className="father">
             <Switch>
-              <Security {...configOkta.oidc}>
-                <Route path="/implicit/callback" component={LoginCallback} />
-                {routeWithSubRoutes(homeRoutes, noop)}
-                <Route component={Main} />
-              </Security>
+              {/* <Security {...configOkta.oidc}>
+                <Route path="/implicit/callback" component={LoginCallback} /> */}
+              {routeWithSubRoutes(homeRoutes, noop)}
+              <Route component={Main} />
+              {/* </Security> */}
             </Switch>
           </div>
         </Router>

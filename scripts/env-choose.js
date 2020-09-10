7edf730ca58900eb2573d31c52fe303fc4de8e1c
YangSheng <kensyang@deloitte.com.cn>
2020-09-10 18:17:02 +0800

    update

--- scripts/env-choose.js
+++ scripts/env-choose.js
@@ -25,19 +25,23 @@ let questions = [
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
+    console.log(process.env.NODE_ENV);
     if (process.argv[2]) {
       console.log(`根据进程参数选择环境:${process.argv[2]}`);
       resolve(process.argv[2]);
       process.env.PORT = portMap[process.argv[2]];
-    } else if (process.env['NODE_ENV:'] === 'production') {
-      resolve('prod');
-    } else {
+    } 
+    // else if (process.env['NODE_ENV'] === 'production') {
+    //   resolve('prod');
+    // } 
+    else {
       require('inquirer')
         .prompt(questions)
         .then(answers => {
           let platform = answers.selection;
           console.log('用户选择环境编码:', platform);
           process.env.PORT = portMap[platform];
+          // process.env.NODE_ENV = nodeEnvMap[platform] || 'production'
           resolve(platform)
         })
     }
@@ -52,3 +56,9 @@ let portMap = {
   local: 3003,
   "dev:205": 3005
 }
+
+// let nodeEnvMap={
+//   dev:'development',
+//   stg:'production_stg',
+//   prod:'production'
+// }

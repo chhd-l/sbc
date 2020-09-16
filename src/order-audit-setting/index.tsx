1e2c157dbad90ca25ea34ec908075ddcecfa8396
YangSheng <kensyang@deloitte.com.cn>
2020-09-16 17:55:15 +0800

    order audit setting

---
+++ src/order-audit-setting/index.tsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { BreadCrumb, Headline, Const, history, AuthWrapper } from 'qmkit';
+import {
+  Icon,
+  Table,
+  Tooltip,
+  Divider,
+  Switch,
+  Modal,
+  Button,
+  Form,
+  Input,
+  Row,
+  Col,
+  Breadcrumb,
+  Tag,
+  message,
+  Select,
+  Radio,
+  DatePicker,
+  Spin,
+  Alert,
+  InputNumber
+} from 'antd';
+
+import * as webapi from './webapi';
+import { FormattedMessage } from 'react-intl';
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+class OrderSetting extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      title: 'Order Audit Setting'
+    };
+  }
+  componentDidMount() {}
+
+  render() {
+    const { title } = this.state;
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <AuthWrapper functionName="fOrderList001_prescriber">
+        <BreadCrumb />
+        {/*导航面包屑*/}
+        <div className="container-search">
+          <Headline title={title} />
+        </div>
+      </AuthWrapper>
+    );
+  }
+}
+
+export default Form.create()(OrderSetting);

3caca4bcae5651db4b687bfd5d2f02ffc48bd5a1
drtan <1@1.com>
2020-09-22 15:14:21 +0800

    20200921a

--- src/shop/components/consent-detail.tsx
+++ src/shop/components/consent-detail.tsx
@@ -195,7 +195,7 @@ export default class StepConsentDetail extends Component<any, any> {
   };
 
   componentDidMount() {
-    const { editId, editList, consentForm } = this.props.relaxProps;
+    const { editId, consentLanguage, consentForm, onFormChange } = this.props.relaxProps;
     if ( editId != '000') {
       this.setState({
         TitleType: this.state.editList.consentTitleType == 'Content' ? true : false,
@@ -203,6 +203,9 @@ export default class StepConsentDetail extends Component<any, any> {
         value: this.state.editList.consentPage.split(',')
 
       });
+    }else {
+      consentForm.languageTypeId = consentLanguage[0].id
+      onFormChange(consentForm)
     }
   }
 

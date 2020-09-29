20fd6c7bbc94ca5b6399897d71f66693a31797b0
drtan <1@1.com>
2020-09-29 12:19:42 +0800

    20200928a

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,13 +1,13 @@
 import Common from './config-common';
 export default {
   // dev
-  HOST: 'http://124.71.151.9:8390',
+  //HOST: 'http://124.71.151.9:8390',
   // HOST: 'https://store.royalcanin.com/api/',
 
 
   // HOST: 'http://192.168.23.25:8087',
   // HOST: 'http://52.168.31.130:8390',
-  //  HOST: 'https://storestg.royalcanin.com/api',
+    HOST: 'https://storestg.royalcanin.com/api',
   // HOST: 'https://store.royalcanin.com/api',
   ...Common
 };

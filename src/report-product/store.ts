c861a543137d3e8114595223239f0f256ea79c90
drtan <1@1.com>
2020-11-02 19:58:30 +0800

    20201026a

--- src/report-product/store.ts
+++ src/report-product/store.ts
@@ -18,15 +18,30 @@ export default class AppStore extends Store {
     return [new reportActor()];
   }
 
-  init = async (param?: any) => {
+  onProductStatistics = async (param?: any) => {
+    this.dispatch('loading:start');
+    const { res } = await webapi.getProductStatistics(param);
+    if (res.code === Const.SUCCESS_CODE) {
+      this.transaction(() => {
+        this.dispatch('loading:end');
+        this.dispatch('report:productStatistics', res.context);
+      });
+    } else {
+      message.error(res.message);
+      if (res.code === 'K-110001') {
+        this.dispatch('loading:end');
+      }
+    }
+  };
+
+  onProductReportPage = async (param?: any) => {
     this.dispatch('loading:start');
     const { res } = await webapi.getProductReportPage(param);
     if (res.code === Const.SUCCESS_CODE) {
-      //param.total = res1.res.context.total
-
       this.transaction(() => {
-        this.dispatch('product:detailProductList', res.context);
-        this.dispatch('product:productselect', arr);
+        this.dispatch('loading:end');
+        this.dispatch('report:productReportPage', res.context);
+        this.dispatch('current', param && param.pageNum + 1);
       });
     } else {
       message.error(res.message);

8d7a31dd31bb0cd02a78c9dd38493f509194baf6
drtan <1@1.com>
2020-11-03 18:58:08 +0800

    20201026a

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,8 +1,8 @@
 import Common from './config-common';
 export default {
   // test-crm
-  HOST: 'http://124.71.151.9:8390',
-  //host: 'https://store.royalcanin.com/api',
+  //HOST: 'http://124.71.151.9:8390',
+  host: 'http://shopstg.royalcanin.com/api',
   //HOST: 'https://store.royalcanin.com/api',
   ...Common
 };

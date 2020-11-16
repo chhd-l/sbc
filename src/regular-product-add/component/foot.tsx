1b2272f46e86afd4be0992700e5cab0a3a2aa7c4
YangXiaoyi <xiaoyiyang@deloitte.com.cn>
2020-11-16 14:08:38 +0800

    product public seo

--- src/regular-product-add/component/foot.tsx
+++ src/regular-product-add/component/foot.tsx
@@ -17,6 +17,7 @@ export default class Foot extends React.Component<any, any> {
       saveLoading: boolean;
       activeTabKey: string;
       onMainTabChange: Function;
+      saveSeoSetting: Function;
     };
   };
 
@@ -26,9 +27,13 @@ export default class Foot extends React.Component<any, any> {
     saveAll: noop,
     saveLoading: 'saveLoading',
     activeTabKey: 'activeTabKey',
-    onMainTabChange: noop
+    onMainTabChange: noop,
+    saveSeoSetting: noop
   };
-
+  _saveSeoSetting = () => {
+    const { saveSeoSetting } = this.props.relaxProps;
+    saveSeoSetting();
+  };
   render() {
     const { saveLoading, activeTabKey } = this.props.relaxProps;
     return (
@@ -57,12 +62,21 @@ export default class Foot extends React.Component<any, any> {
               Next
             </Button>
           </AuthWrapper>
-        ) : (
+        ) : this.props.tabType == 'related' ? (
           <AuthWrapper key="004" functionName={this.props.priceFuncName}>
             <Button type="primary" onClick={() => this._prev(this.props.tabType)} style={{ marginRight: 10 }} loading={saveLoading}>
               Prev
             </Button>
-            <Button type="primary" onClick={this._savePrice} style={{ marginRight: 10 }} loading={saveLoading}>
+            <Button type="primary" onClick={() => this._next(this.props.tabType)} style={{ marginRight: 10 }} loading={saveLoading}>
+              Next
+            </Button>
+          </AuthWrapper>
+        ) : (
+          <AuthWrapper key="005" functionName={this.props.priceFuncName}>
+            <Button type="primary" onClick={() => this._prev(this.props.tabType)} style={{ marginRight: 10 }} loading={saveLoading}>
+              Prev
+            </Button>
+            <Button type="primary" onClick={this._saveSeoSetting} style={{ marginRight: 10 }} loading={saveLoading}>
               Save
             </Button>
           </AuthWrapper>

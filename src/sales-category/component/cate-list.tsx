c6fe31ad887e48fab7473b3439b01959649447e8
YangSheng <kensyang@deloitte.com.cn>
2020-12-10 10:00:46 +0800

    拖拽 暂存

--- src/sales-category/component/cate-list.tsx
+++ src/sales-category/component/cate-list.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { Relax } from 'plume2';
 import { List, Map, fromJS } from 'immutable';
 import { Form, Modal, Popconfirm, Switch, Table, Tooltip } from 'antd';
-// import { DragDropContext, DragSource, DropTarget } from 'react-dnd';
-// import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext, DragSource, DropTarget } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import { noop, checkAuth } from 'qmkit';
 import { FormattedMessage } from 'react-intl';
 
@@ -17,7 +17,7 @@ const styles = {
 };
 
 @Relax
-export default class CateList extends React.Component<any, any> {
+class CateList extends React.Component<any, any> {
   props: {
     relaxProps?: {
       dataList: IList;
@@ -64,21 +64,21 @@ export default class CateList extends React.Component<any, any> {
         rowKey="storeCateId"
         columns={this._columns}
         dataSource={dataList.toJS()}
-        // components={this.components}
+        components={this.components}
         pagination={false}
-        // onRow={(_record, index) => ({
-        //   index,
-        //   moveRow: this._moveRow
-        // })}
+        onRow={(_record, index) => ({
+          index,
+          moveRow: this._moveRow
+        })}
       />
     );
   }
 
-  // components = {
-  //   body: {
-  //     row: _BodyRow
-  //   }
-  // };
+  components = {
+    body: {
+      row: _BodyRow
+    }
+  };
 
   _columns = [
     {
@@ -293,109 +293,85 @@ export default class CateList extends React.Component<any, any> {
    * @param hoverIndex 拖拽排序目标位置
    * @private
    */
-  // _moveRow = (catePath, dragIndex, hoverIndex) => {
-  //   const { cateSort } = this.props.relaxProps;
-  //   cateSort(catePath, dragIndex, hoverIndex);
-  // };
+  _moveRow = (catePath, dragIndex, hoverIndex) => {
+    const { cateSort } = this.props.relaxProps;
+    cateSort(catePath, dragIndex, hoverIndex);
+  };
 }
 
-// let _dragDirection = (
-//   dragIndex,
-//   hoverIndex,
-//   initialClientOffset,
-//   clientOffset,
-//   sourceClientOffset
-// ) => {
-//   const hoverMiddleY = (initialClientOffset.y - sourceClientOffset.y) / 2;
-//   const hoverClientY = clientOffset.y - sourceClientOffset.y;
-//   if (dragIndex < hoverIndex && hoverClientY > hoverMiddleY) {
-//     return 'downward';
-//   }
-//   if (dragIndex > hoverIndex && hoverClientY < hoverMiddleY) {
-//     return 'upward';
-//   }
-// };
+let _dragDirection = (dragIndex, hoverIndex, initialClientOffset, clientOffset, sourceClientOffset) => {
+  const hoverMiddleY = (initialClientOffset.y - sourceClientOffset.y) / 2;
+  const hoverClientY = clientOffset.y - sourceClientOffset.y;
+  if (dragIndex < hoverIndex && hoverClientY > hoverMiddleY) {
+    return 'downward';
+  }
+  if (dragIndex > hoverIndex && hoverClientY < hoverMiddleY) {
+    return 'upward';
+  }
+};
 
-// let _BodyRow = (props) => {
-//   const {
-//     isOver,
-//     connectDragSource,
-//     connectDropTarget,
-//     moveRow,
-//     dragRow,
-//     clientOffset,
-//     sourceClientOffset,
-//     initialClientOffset,
-//     ...restProps
-//   } = props;
-//   const style = { ...restProps.style, cursor: 'move' };
-//   let className = restProps.className;
-//   if (isOver && initialClientOffset) {
-//     const direction = _dragDirection(
-//       dragRow.index,
-//       restProps.index,
-//       initialClientOffset,
-//       clientOffset,
-//       sourceClientOffset
-//     );
-//     if (direction === 'downward') {
-//       className += ' drop-over-downward';
-//     }
-//     if (direction === 'upward') {
-//       className += ' drop-over-upward';
-//     }
-//   }
-//   return connectDragSource(
-//     connectDropTarget(<tr {...restProps} className={className} style={style} />)
-//   );
-// };
+let _BodyRow = (props) => {
+  const { isOver, connectDragSource, connectDropTarget, moveRow, dragRow, clientOffset, sourceClientOffset, initialClientOffset, ...restProps } = props;
+  const style = { ...restProps.style, cursor: 'move' };
+  let className = restProps.className;
+  if (isOver && initialClientOffset) {
+    const direction = _dragDirection(dragRow.index, restProps.index, initialClientOffset, clientOffset, sourceClientOffset);
+    if (direction === 'downward') {
+      className += ' drop-over-downward';
+    }
+    if (direction === 'upward') {
+      className += ' drop-over-upward';
+    }
+  }
+  return connectDragSource(connectDropTarget(<tr {...restProps} className={className} style={style} />));
+};
 
-// const _rowSource = {
-//   beginDrag(props) {
-//     return {
-//       index: props.index,
-//       cateInfo: props.children[0].props.record
-//     };
-//   }
-// };
+const _rowSource = {
+  beginDrag(props) {
+    return {
+      index: props.index,
+      cateInfo: props.children[0].props.record
+    };
+  }
+};
 
-// const _rowTarget = {
-//   drop(props, monitor) {
-//     //源对象
-//     const sourceCate = monitor.getItem().cateInfo;
-//     //目标对象
-//     const targetCate = props.children[0].props.record;
-//     //判断两个拖拽目标是不是同一级
-//     if (sourceCate.cateParentId != targetCate.cateParentId) {
-//       return;
-//     }
-//     //默认分类不能排序
-//     if (sourceCate.isDefault == 1 || targetCate.isDefault == 1) {
-//       return;
-//     }
-//     const dragIndex = monitor.getItem().index;
-//     const hoverIndex = props.index;
-//     //自己不能和自己换位置
-//     if (dragIndex === hoverIndex) {
-//       return;
-//     }
-//     //拖拽排序方法
-//     props.moveRow(sourceCate.catePath, dragIndex, hoverIndex);
-//     monitor.getItem().index = hoverIndex;
-//   }
-// };
+const _rowTarget = {
+  drop(props, monitor) {
+    //源对象
+    const sourceCate = monitor.getItem().cateInfo;
+    //目标对象
+    const targetCate = props.children[0].props.record;
+    //判断两个拖拽目标是不是同一级
+    if (sourceCate.cateParentId != targetCate.cateParentId) {
+      return;
+    }
+    //默认分类不能排序
+    if (sourceCate.isDefault == 1 || targetCate.isDefault == 1) {
+      return;
+    }
+    const dragIndex = monitor.getItem().index;
+    const hoverIndex = props.index;
+    //自己不能和自己换位置
+    if (dragIndex === hoverIndex) {
+      return;
+    }
+    //拖拽排序方法
+    props.moveRow(sourceCate.catePath, dragIndex, hoverIndex);
+    monitor.getItem().index = hoverIndex;
+  }
+};
 
-// _BodyRow = DropTarget('row', _rowTarget, (connect, monitor) => ({
-//   connectDropTarget: connect.dropTarget(),
-//   isOver: monitor.isOver(),
-//   sourceClientOffset: monitor.getSourceClientOffset()
-// }))(
-//   DragSource('row', _rowSource, (connect, monitor) => ({
-//     connectDragSource: connect.dragSource(),
-//     dragRow: monitor.getItem(),
-//     clientOffset: monitor.getClientOffset(),
-//     initialClientOffset: monitor.getInitialClientOffset()
-//   }))(_BodyRow)
-// );
+_BodyRow = DropTarget('row', _rowTarget, (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  isOver: monitor.isOver(),
+  sourceClientOffset: monitor.getSourceClientOffset()
+}))(
+  DragSource('row', _rowSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    dragRow: monitor.getItem(),
+    clientOffset: monitor.getClientOffset(),
+    initialClientOffset: monitor.getInitialClientOffset()
+  }))(_BodyRow)
+);
 
-// export default DragDropContext(HTML5Backend)(CateList);
+export default DragDropContext(HTML5Backend)(CateList);

3c2e6ba74f53b8c78f4c2c24c46dcd757f9d32da
chuali <chuali@deioitte.com.cn>
2020-12-16 14:17:25 +0800

    product bug

--- src/regular-product-add/component/detail.tsx
+++ src/regular-product-add/component/detail.tsx
@@ -50,6 +50,7 @@ export default class Detail extends React.Component<any, any> {
 
     if (goods.get('goodsDetail')) {
       goodsDetailContent = goods.get('goodsDetail');
+      debugger
       try {
         goodsDetailTabContent = JSON.parse(goods.get('goodsDetail'));
       } catch {
@@ -77,7 +78,8 @@ export default class Detail extends React.Component<any, any> {
                     height="320"
                     // content="112"
                     // content = {JSON.parse(goods.get('goodsDetail'))[item.get('name')]}
-                    content={goodsDetailTabContent && goodsDetailTabContent[item.get('name')]}
+                    content={goodsDetailTabContent && goodsDetailTabContent[item.get('name')] ?
+                     (JSON.stringify(goodsDetailTabContent[item.get('name')])).replace(/^\"|\"$/g,'') : ''} //去除前后的双引号
                     insertImg={() => {
                       this._handleClick();
                       this.props.relaxProps.editEditor('detail');

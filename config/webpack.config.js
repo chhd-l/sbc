33236227f492ba2964f59fea2bece50aa2e8a282
chuali <chuali@deioitte.com.cn>
2020-12-16 20:56:35 +0800

    new build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -54,7 +54,7 @@ module.exports = function (webpackEnv, envCode = 'prod') {
   const isEnvProduction = envCode === 'prod';
 
   const publicPath = isEnvProduction
-    ? "https://cdnstorestg.azureedge.net/res/"
+    ? "./"
     : isEnvDevelopment && './';
   const shouldUseRelativeAssetPaths = publicPath === './';
 

7bf9b5facaade95de8c41d4a0e75b036507b0d18
chuali <chuali@deioitte.com.cn>
2021-01-08 11:18:40 +0800

    subscription-plan-update

--- src/subscription-plan-update/components/entryCriteria.tsx
+++ src/subscription-plan-update/components/entryCriteria.tsx
@@ -1,16 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Form, Input, Checkbox, Row, Col } from 'antd';
+const FormItem = Form.Item;
 
 export default class entryCriteria extends Component<any, any> {
-    constructor(props) {
-        super(props);
-        this.state = {};
-      }
-    render() {
-        return (
-            <div>
-                                <h3>Step3</h3>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { subscriptionPlan, addField } = this.props;
+    return (
+      <div>
+        <h3>Step3</h3>
         <h4>Entry Criteria</h4>
-            </div>
-        )
-    }
+        <div className="entryCriteria">
+          <Form>
+            <FormItem>
+              <Checkbox
+                checked={subscriptionPlan.signOnSubscription === 1 ? true : false}
+                onChange={(e) => {
+                  subscriptionPlan.signOnSubscription = e.target.checked ? 1 : 0;
+                  addField('signOnSubscription', subscriptionPlan.signOnSubscription);
+                }}
+              >
+                Sign On Subscription
+              </Checkbox>
+              <span className="checkBoxTip">Consumers need consent to sign on subscription</span>
+            </FormItem>
+            <FormItem>
+              <Checkbox
+                checked={subscriptionPlan.otherPromotion === 1 ? true : false}
+                onChange={(e) => {
+                  subscriptionPlan.otherPromotion = e.target.checked ? 1 : 0;
+                  addField('signOnSubscription', subscriptionPlan.otherPromotion);
+                }}
+              >
+                Subscription Plan
+              </Checkbox>
+              <span className="checkBoxTip">Subscription plan can be applied with other promotions</span>
+            </FormItem>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }

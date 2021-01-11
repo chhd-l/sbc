fdfe02cfa0eb8ca28a8d847d77bf5d65a5f41afc
Zr3Lm9Yh <18315151342@163.com>
2021-01-11 12:56:27 +0800

    1-11bug改1

--- src/coupon-activity-add/specify/components/specify-customer.tsx
+++ src/coupon-activity-add/specify/components/specify-customer.tsx
@@ -38,12 +38,12 @@ export default class ChooseCustomer extends React.Component<any, any> {
           {chooseCustomerList &&
             chooseCustomerList.map((record) => {
               return (
-                <Col span={4} key={record.customerId}>
-                  <p style={styles.item}>
+                <Col key={record.customerId}>
+                  <p style={{ justifyContent: 'space-around', flex: 1, marginLeft: '5px' }}>
                     {/* {record.customerName}
                   &nbsp; */}
                     {record.customerAccount}
-                    <a style={{ margin: 10 }} href="javascript:void(0)" onClick={() => onDelCustomer(record.customerId)}>
+                    <a style={{ marginLeft: '5px' }} href="javascript:void(0)" onClick={() => onDelCustomer(record.customerId)}>
                       删除
                     </a>
                   </p>

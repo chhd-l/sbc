14807b2a91f067215e417cc3879090dd442a62f1
chuali <chuali@deioitte.com.cn>
2021-01-16 21:51:47 +0800

    api config

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -4,10 +4,10 @@ export default {
   // HOST: 'http://124.71.151.9:8390',
 
   /* stg */
-  HOST: 'https://storestg.royalcanin.com/api',
+  // HOST: 'https://storestg.royalcanin.com/api',
 
   /* product */
-  //HOST: 'http://eurostore.royalcanin.com/api',
+  HOST: 'http://eurostore.royalcanin.com/api',
 
 
   CDN_PATH: '',

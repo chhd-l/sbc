6f6cf04cfc14bfca47880d5427f460b5cdecb85f
chuali <chuali@deioitte.com.cn>
2021-01-18 20:27:09 +0800

    euro okta config

--- web_modules/qmkit/config-common.ts
+++ web_modules/qmkit/config-common.ts
@@ -159,12 +159,7 @@ const Common = {
   // REACT_APP_RC_RedirectURL: window.origin +  "/implicit/callback",
 
   // PROD MX RC STAFF Okta Config
-  REACT_APP_RC_CLIENT_ID: "0oa5odnbjhRhbV16X357",
-  REACT_APP_RC_ISSUER : "https://mars-group.okta.com",
-  REACT_APP_RC_RedirectURL: window.origin + "/implicit/callback?type=staff",
-
-  // STG RC STAFF Okta Config
-  // REACT_APP_RC_CLIENT_ID: "0oa6fb12ahvn5lAAL357",
+  // REACT_APP_RC_CLIENT_ID: "0oa5odnbjhRhbV16X357",
   // REACT_APP_RC_ISSUER : "https://mars-group.okta.com",
   // REACT_APP_RC_RedirectURL: window.origin + "/implicit/callback?type=staff",
 

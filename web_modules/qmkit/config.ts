39593b2cbac84217296c120ec67a12db3110388b
drtan <1>
2021-01-18 20:00:34 +0800

    2021018a

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,10 +1,7 @@
 import Common from './config-common';
 export default {
-  /* dev */
-  HOST: 'http://124.71.151.9:8390',
-
-  /* stg */
-  //HOST: 'https://storestg.royalcanin.com/api',
-  CDN_PATH: 'https://cdnstorestg.azureedge.net/res/',
+  HOST: 'https://eurostore.royalcanin.com/api',
+  CDN_PATH: '/',
+  //CDN_PATH: 'https://d2cde.azureedge.net/rs/',
   ...Common
 };

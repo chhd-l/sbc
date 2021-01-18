e33a2ec19fba8ebb9cfca5744b377c01e2221a88
cjgsky <267003901@qq.com>
2021-01-18 15:46:22 +0800

    修改https

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,6 +1,6 @@
 import Common from './config-common';
 export default {
-  HOST: 'http://eurostore.royalcanin.com/api',
+  HOST: 'https://eurostore.royalcanin.com/api',
   CDN_PATH: '',
   //CDN_PATH: 'https://d2cde.azureedge.net/rs/',
   ...Common

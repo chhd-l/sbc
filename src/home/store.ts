8bc64a765bf7ea8b8910b7fa246bbf020841c9ee
YXYFuture <1247676987@qq.com>
2021-01-19 16:29:47 +0800

    home

--- src/home/store.ts
+++ src/home/store.ts
@@ -127,14 +127,14 @@ export default class AppStore extends Store {
     const { res } = await webapi.getStoreDashboardCollectViewstore(data);
     if (res.code == Const.SUCCESS_CODE) {
       this.dispatch('loading:end');
-      this.dispatch('home:tradeCustomerView', res.context.tradeCustomerView);
+      this.dispatch('home:tradeCustomerView', res.context ? res.context.tradeCustomerView : null);
       this.dispatch('home:goodsInfoTopView', res.context.goodsInfoTopView.goodsInfoTopViewItemList || []);
-      this.dispatch('home:conversionFunnelDashboardView', res.context.conversionFunnelDashboardView);
-      this.dispatch('home:prescriberTrendView', res.context.prescriberTrendView);
-      this.dispatch('home:prescriberTopView', res.context.prescriberTopView);
-      this.dispatch('home:trafficDashboardView', res.context.trafficDashboardView);
-      this.dispatch('home:transactionTrendView', res.context.transactionTrendView);
-      this.dispatch('home:trafficTrendDashboardView', res.context.trafficTrendDashboardView);
+      this.dispatch('home:conversionFunnelDashboardView', res.context ? res.context.conversionFunnelDashboardView : null);
+      this.dispatch('home:prescriberTrendView', res.context ? res.context.prescriberTrendView : null);
+      this.dispatch('home:prescriberTopView', res.context ? res.context.prescriberTopView : null);
+      this.dispatch('home:trafficDashboardView', res.context ? res.context.trafficDashboardView : null);
+      this.dispatch('home:transactionTrendView', res.context ? res.context.transactionTrendView : null);
+      this.dispatch('home:trafficTrendDashboardView', res.context ? res.context.trafficTrendDashboardView : null);
       //this.dispatch('home:searchData', getListAll.context);
     } else {
       this.dispatch('loading:end');

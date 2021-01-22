db421542304f1ceb7c63137fddb7ccd8a91d087d
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-01-22 18:23:04 +0800

    description management searching form

--- src/description-management/components/search-form.tsx
+++ src/description-management/components/search-form.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Row, Col, Form, Input, Button } from 'antd';
+import { FormattedMessage } from 'react-intl';
 
 type Iprop = {
   descName: string;
   onSearch?: Function;
-  form?: object;
 };
 
 class SearchForm extends Component<Iprop> {
@@ -24,6 +24,43 @@ class SearchForm extends Component<Iprop> {
       }
     });
   };
+
+  onFormSearch = () => {};
+
+  render() {
+    return (
+      <Form layout="inline" style={{ marginBottom: 20 }}>
+        <Row>
+          <Col span={8}>
+            <Form.Item>
+              <Input
+                addonBefore="Description name"
+                onChange={(e) => {
+                  const value = (e.target as any).value;
+                  this.onFieldChange(value);
+                }}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Button
+              type="primary"
+              icon="search"
+              shape="round"
+              onClick={(e) => {
+                e.preventDefault();
+                this.onFormSearch();
+              }}
+            >
+              <span>
+                <FormattedMessage id="search" />
+              </span>
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
 }
 
 export default Form.create()(SearchForm);

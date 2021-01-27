2dae41c3f87525edf0c268e910ca11b14a17e2a7
YXYFuture <1247676987@qq.com>
2021-01-27 15:38:17 +0800

    coupon

--- web_modules/qmkit/fetch/HttpUtil.ts
+++ web_modules/qmkit/fetch/HttpUtil.ts
@@ -68,7 +68,7 @@ class HttpUtil{
                                 message.info(msg)
                                 reject(HttpUtil.handleFailedResult({ fetchStatus: response.status, netStatus: response.status, error: msg }, httpCustomerOpertion))
                             }
-                       
+
                     }).catch(e => {
                         const errMsg = e.name + " " + e.message
                         reject(HttpUtil.handleFailedResult({ fetchStatus: response.status, error: errMsg, netStatus: response.status }, httpCustomerOpertion))
@@ -91,10 +91,10 @@ class HttpUtil{
     /**
        * 统一处理后台返回的结果, 包括业务逻辑报错的结果
        * @param result
-       * 
+       *
        */
     static handleResult(result, httpCustomerOpertion) {
-        
+
             let code = result.code
             if (code && httpCustomerOpertion.isHandleResult === true) {
                 const errMsg = result.msg || result.message || "Service is busy,please try again later"
@@ -107,10 +107,10 @@ class HttpUtil{
     /**
      * 统一处fetch的异常, 不包括业务逻辑报错
      * @param result
-     * 
+     *
      */
     static handleFailedResult(result, httpCustomerOpertion) {
-        
+
             if (result.code && httpCustomerOpertion.isHandleResult === true) {
                 const errMsg = result.msg || result.message || "Service is busy,please try again later"
                 const errStr = `${errMsg}（${result.code}）`
@@ -122,7 +122,7 @@ class HttpUtil{
            process.env.NODE_ENV==="production"&&history.push('error')
 
             return errorMsg
-       
+
 
     }
     /**

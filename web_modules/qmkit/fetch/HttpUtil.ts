e68d2af8d4035f92f8f79cf720f2166de1b31b16
CHARLE CAI <ccharsle@gmail.com>
2021-02-01 15:46:40 +0800

    修改容错处理

--- web_modules/qmkit/fetch/HttpUtil.ts
+++ web_modules/qmkit/fetch/HttpUtil.ts
@@ -1,12 +1,13 @@
 import { message } from 'antd';
 import { Store } from 'plume2';
-import { util, history,cache } from 'qmkit';
+import { util, history, cache } from 'qmkit';
 const msg = {
     'K-000005': 'Your account is disabled',
     'K-000015': 'Failed to obtain authorization',
     'K-000002': ''
-}
-class HttpUtil{
+};
+let errorList: any = [], errorObj: any = {}, _timerOut: any = null, _times: number = 0,_error_index=0;
+class HttpUtil {
     /**
       * 发送fetch请求
        * @param fetchUrl
@@ -15,6 +16,7 @@ class HttpUtil{
        */
 
     static handleFetchData(fetchUrl, fetchParams, httpCustomerOpertion) {
+        errorObj = Object.assign({}, { fetchUrl: fetchUrl.split('?')[0], fetchParams, httpCustomerOpertion });
         // 1. 处理的第一步
         const { isShowLoading } = httpCustomerOpertion
         if (isShowLoading) {
@@ -41,44 +43,46 @@ class HttpUtil{
                     }
                     httpCustomerOpertion.isFetched = true
                     response.json().then(jsonBody => {
-                            if (response.status == 200 && response.ok) {
-                                if (jsonBody.code === 'K-999996') {
-                                    message.error(jsonBody.message);
-                                    return;
-                                }
-                                // token 过期时，前端直接处理
-                                else if (jsonBody.code === 'K-000002') {
-                                    message.error(jsonBody.message);
-                                    util.logout();
-                                    history.push('/login');
-                                }
-                                // 账号禁用 统一返回到登录页面
-                                else if (['K-000005', 'K-000015'].includes(jsonBody.code)) {
-                                    message.error(msg[jsonBody.code]);
-                                    history.push('login', { oktaLogout: true })
-                                    return;
-                                } else if (jsonBody === 'Method Not Allowed') {
-                                    message.error('You do not have permission to access this feature');
-                                    return;
-                                } else {
-                                    resolve(HttpUtil.handleResult(jsonBody, httpCustomerOpertion))
-                                }
-                            } else {
-                                // 5. 接口状态判断
-                                // http status header <200 || >299
-                                let msg = "Service is busy,please try again later"
-                                if (response.status === 404) {
-                                    msg = jsonBody.message
-                                }
-
-                               // message.info(msg)
-                                reject(HttpUtil.handleFailedResult({code: response.status,message:msg,  error: msg }, httpCustomerOpertion))
+                        if (response.status == 200 && response.ok) {
+                            _error_index=0;
+                            HttpUtil.findErrorInterfaceReload(true, fetchUrl)
+                            if (jsonBody.code === 'K-999996') {
+                                message.error(jsonBody.message);
+                                return;
                             }
+                            // token 过期时，前端直接处理
+                            else if (jsonBody.code === 'K-000002') {
+                                message.error(jsonBody.message);
+                                util.logout();
+                                history.push('/login');
+                            }
+                            // 账号禁用 统一返回到登录页面
+                            else if (['K-000005', 'K-000015'].includes(jsonBody.code)) {
+                                message.error(msg[jsonBody.code]);
+                                history.push('login', { oktaLogout: true })
+                                return;
+                            } else if (jsonBody === 'Method Not Allowed') {
+                                message.error('You do not have permission to access this feature');
+                                return;
+                            } else {
+                                resolve(HttpUtil.handleResult(jsonBody, httpCustomerOpertion))
+                            }
+                        } else {
+                            // 5. 接口状态判断
+                            // http status header <200 || >299
+                            let msg = "Service is busy,please try again later"
+                            if (response.status === 404) {
+                                msg = jsonBody.message
+                            }
+
+                            // message.info(msg)
+                            reject(HttpUtil.handleFailedResult({ code: response.status, message: msg, error: msg }, httpCustomerOpertion))
+                        }
 
                     }).catch(e => {
                         let msg = "Service is busy,please try again later"
                         const errMsg = e.name + " " + e.message
-                        reject(HttpUtil.handleFailedResult({ code: response.status,message:msg, error: errMsg,  }, httpCustomerOpertion))
+                        reject(HttpUtil.handleFailedResult({ code: response.status, message: msg, error: errMsg, }, httpCustomerOpertion))
                     })
                 }
             ).catch(e => {
@@ -88,7 +92,8 @@ class HttpUtil{
                     return
                 }
                 httpCustomerOpertion.isFetched = true
-                let er={ code: "404", error: errMsg ,message:'Request interface failed or interface does not exist, please check it'}
+                let er = { code: "404", error: errMsg, message: 'Request interface failed or interface does not exist, please check it' }
+                HttpUtil.findErrorInterfaceReload(false, fetchUrl.split('?')[0])
                 reject(HttpUtil.handleFailedResult(er, httpCustomerOpertion))
             })
         })
@@ -101,15 +106,15 @@ class HttpUtil{
        */
     static handleResult(result, httpCustomerOpertion) {
 
-            let code = result.code
-            if (code && httpCustomerOpertion.isHandleResult === true) {
-                const errMsg = result.msg || result.message || "Service is busy,please try again later"
-                const errStr = `${errMsg}`
-                //message.success(errStr)
-            }
-
-            return result
+        let code = result?.code??false;
+        if (code && httpCustomerOpertion.isHandleResult === true) {
+            const errMsg = result.msg || result.message || "Service is busy,please try again later"
+            const errStr = `${errMsg}`
+            //message.success(errStr)
         }
+
+        return result
+    }
     /**
      * 统一处fetch的异常, 不包括业务逻辑报错
      * @param result
@@ -117,17 +122,18 @@ class HttpUtil{
      */
     static handleFailedResult(result, httpCustomerOpertion) {
 
-            if (result.code && httpCustomerOpertion.isHandleResult === true) {
-                const errMsg = result.msg || result.message || "Service is busy,please try again later"
-                const errStr = `${errMsg}（${result.code}）`
-                // HttpUtil.hideLoading()
-                message.info(errStr)
-            }
-            // const errorMsg = "Uncaught PromiseError: " + (result.netStatus || "") + " " + (result.error || result.msg || result.message || "")
-            // sessionStorage.setItem(cache.ERROR_INFO,JSON.stringify({...result,error:errorMsg}))
-           // process.env.NODE_ENV==="production"&&history.push('/error')
+        if (result.code && httpCustomerOpertion.isHandleResult === true) {
+            const errMsg = result.msg || result.message || "Service is busy,please try again later"
+            const errStr = `${errMsg}（${result.code}）`
+            // HttpUtil.hideLoading()
+            _error_index===0&&message.info(errStr)
+        }
+        _error_index++;
+        // const errorMsg = "Uncaught PromiseError: " + (result.netStatus || "") + " " + (result.error || result.msg || result.message || "")
+        // sessionStorage.setItem(cache.ERROR_INFO,JSON.stringify({...result,error:errorMsg}))
+        // process.env.NODE_ENV==="production"&&history.push('/error')
 
-            return result;
+        return result;
     }
     /**
      * 控制Fetch请求是否超时
@@ -142,12 +148,50 @@ class HttpUtil{
                     httpCustomerOpertion.isAbort = true
 
                     message.info("Service is busy,please try again later")
-                    reject({ fetchStatus: "timeout" })
+                    reject({ code: "timeout" })
                 }
             }, httpCustomerOpertion.timeout || 100000)
         })
     }
 
+
+    /**
+     * 
+     * @param status 请求成功的状态
+     */
+    static findErrorInterfaceReload(status, fetchUrl: any) {
+        let _index = errorList.findIndex(item => item.fetchUrl === fetchUrl)
+
+        if (!status && _index === -1) {
+            errorList.push(errorObj);
+        } else if (status && _index > -1) {
+            errorList.splice(_index, 1)
+        }
+        // return errorList;
+        const reoloadApi = () => {
+            _timerOut = setTimeout(() => {
+                let {
+                    fetchUrl,
+                    fetchParams,
+                    httpCustomerOpertion
+                } = errorList[_times];
+                if (typeof fetchUrl === 'string') {
+                    fetchUrl += `${fetchUrl.indexOf('?') == -1 ? '?reqId=' : '&reqId='
+                        }${Math.random()}`;
+                }
+                if (errorList.length === _times) {
+                    _times = 0;
+                    HttpUtil.handleFetchData(fetchUrl, fetchParams, httpCustomerOpertion);
+                    clearTimeout(_timerOut)
+                } else {
+                    HttpUtil.handleFetchData(fetchUrl, fetchParams, httpCustomerOpertion);
+                    _times++;
+                }
+            }, 1000);
+        }
+        reoloadApi();
+
+    }
 
 }
 export default HttpUtil;

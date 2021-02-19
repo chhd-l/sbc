8961fdfc5b2c4772f7522a11dcd1d0be0e0b1f54
chuali <chuali@deioitte.com.cn>
2021-02-19 18:16:36 +0800

    email filter

--- src/pet-owner-activity/components/emails.tsx
+++ src/pet-owner-activity/components/emails.tsx
@@ -13,7 +13,7 @@ export default class emails extends Component<any, any> {
       emailLoading: false,
       emailList: [
         {
-          activityName: 'New booking in /\'clinics name\' from /\'2021-01-29T09:31 to /\'2021-01-29T10:31\'',
+          activityName: "New booking in /'clinics name' from /'2021-01-29T09:31 to /'2021-01-29T10:31'",
           activityType: 'Clinic Booking',
           auditId: 8018,
           category: 'Clinic',
@@ -26,44 +26,58 @@ export default class emails extends Component<any, any> {
           id: 8196,
           tenantId: 4
         }
-      ]
+      ],
+      emailFilter: [
+        { value: 'COMMUNICATION.Emails', label: 'Communication Email' },
+        { value: 'CAMPAIGN ACTIVITY.Emails', label: 'Campaign Email' }
+      ],
+      filterVisible: false
     };
     this.activitiesEmailSort = this.activitiesEmailSort.bind(this);
   }
+
+  componentDidMount() {}
+
   activitiesEmailSort() {}
   render() {
-    const { emailLoading, emailList } = this.state;
+    const { emailLoading, emailList, emailFilter, filterVisible } = this.state;
     const menu = (
       <Menu>
         <Menu.Item key={1}>Add Comment</Menu.Item>
         <Menu.Item key={2}>Add Task</Menu.Item>
       </Menu>
     );
+    const filterMenu = (
+      <Menu>
+        <Checkbox>Select All</Checkbox>
+        <a className="closeFilter" onClick={() => this.setState({ filterVisible: false })}>
+          {' '}
+          X
+        </a>
+        <Divider />
+        <CheckboxGroup>
+          {emailFilter.map((item, index) => (
+            <Row gutter={24} key={index}>
+              <Col span={24}>
+                <Checkbox value={item.value}>{item.label}</Checkbox>
+              </Col>
+            </Row>
+          ))}
+        </CheckboxGroup>
+      </Menu>
+    );
     return (
       <Row>
         <Col span={9}></Col>
         <Col span={15} className="activities-right" style={{ marginBottom: '20px' }}>
-          {/* <Dropdown trigger={['click']} overlayClassName="dropdown-custom" style={{marginRight: '10px'}}>
-                    <Button className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
-                      Email Type
-                      <Icon type="down" />
-                    </Button>
-                    <Menu slot="overlay">
-                      <Checkbox
-                        :indeterminate="indeterminateEmail"
-                        @change="onCheckAllEamil"
-                        :checked="checkEmailAll"
-                      >{{ $t('public.selectAll') }}</Checkbox>
-                      <Divider />
-                      <CheckboxGroup v-model="emailCheckedList" @change="emailChange">
-                        <Row :gutter="24" v-for="(item, i) in emailFilter" :key="i">
-                          <Col span={24}>
-                            <Checkbox :value="item.value">{{ item.label }}</Checkbox>
-                          </Col>
-                        </Row>
-                      </CheckboxGroup>
-                    </Menu>
-                  </Dropdown> */}
+          <div style={{ marginRight: '10px' }}>
+            <Dropdown overlay={filterMenu} trigger={['click']} overlayClassName="dropdown-custom" visible={filterVisible}>
+              <Button className="ant-dropdown-link" onClick={(e) => this.setState({ filterVisible: true })}>
+                Email Type
+                <Icon type="down" />
+              </Button>
+            </Dropdown>
+          </div>
           <Button className="sortBtn" onClick={this.activitiesEmailSort}>
             <span className="icon iconfont iconbianzu" style={{ fontSize: '22px' }} />
           </Button>

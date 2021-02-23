c0961a8fe0cd63d197bd03b40c84d9277d3919b5
YangSheng <kensyang@deloitte.com.cn>
2021-02-23 17:38:13 +0800

    setting-tax/setting-address

--- src/router.tsx
+++ src/router.tsx
@@ -906,6 +906,10 @@ const routes = [
   {
     path: '/prescriber-booking-detail',
     asyncComponent: () => import('./prescriber-booking-detail')
+  },
+  {
+    path: '/validation-setting',
+    asyncComponent: () => import('./validation-setting')
   }
 ];
 

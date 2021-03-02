82aaf5628125deac4d1ac1a6fe5f7f018ab9add0
CHARLE CAI <ccharsle@gmail.com>
2021-03-02 18:16:19 +0800

    添加product翻译

--- web_modules/qmkit/es_ES.ts
+++ web_modules/qmkit/es_ES.ts
@@ -39,27 +39,27 @@ export default {
   'Home.TransactionTrend':'Transaction Trend',
   'Home.Prescriberreward':'Prescriber reward',
   'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
-  'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
+  // 'Home.':'',
 
 
   /*-----------------------------  Product  ------------------------------------------*/
@@ -81,8 +81,9 @@ export default {
   "Product.Batchonshelves": "Batch on shelves",
   "Product.Batchoffshelves": "Batch off shelves",
   "Product.Batchdelete": "Batch delete",
-
-
+  "Product.DeleteConfirmTip": "Are you sure you want to delete this product?",
+  "Product.OffshelvesConfirmTip": "Are you sure you want off to shelves this product?",
+ 
 
   "Product.Pending": "Pending",
   "Product.Pendingreview": "Pending review",
@@ -110,13 +111,9 @@ export default {
   "Product.Relatedproduct": "Related product",
   "Product.SEOsetting": "SEO setting",
   "Product.Operationinstruction": `Operation instruction：
-
   1、Until filled all required fields, the product could be published.
-  
   2、 The product information tab, product price tab, product inventory tab must be filled before published.
-  
   3、If you do not need to set the related product or SEO, you can click 'Next' directly after editing the inventory information.`,
-
   "Product.InternalSPU": "Internal SPU",
   "Product.Subscriptionstatus": "Subscription status",
   "Product.Productcardintro": "Product card intro",
@@ -145,15 +142,10 @@ export default {
   "Product.Inventory Alert": "Inventory Alert",
 
   "Product.SEOsettingalert": `You can delete these fields, including { name }-Royal Canin, { name }, { subtitle }, { sales category }, { tagging }, { description }, but can not edit the field
-
     1、{ name } means the name of the product which enters in the product information tab page
-    
     2、{subtitle } means the subtitle of the product which enters in the product information tab page
-    
     3、{ sales category } means the sales category of the product which was chosen in the product information tab page
-    
     4、{ tagging } means the sales category of the product which was chosen in the product information
-    
     5、{description } means the description of the product which enters in the product information tab page`,
   "Product.MetaKeywords": "Meta Keywords",
   "Product.MetaDescription": "Meta Description",
@@ -188,6 +180,27 @@ export default {
   "Product.Descriptiontitle": "Description title",
   "Product.Categorydescription": "Category description",
   "Product.Categorydescriptionplaceholder": "Please input the product description",
+
+  "Product.Commoditylibraryimport":"Commodity library import",
+  "Product.Instructions":"Instructions",
+  "Product.Instructionstip":"The products imported from the commodity library are initially reviewed / off the shelf. Please add the information before selling them;",
+  "Product.Signedclassification":"Signed classification",
+  "Product.BatchImport":"Batch Import",
+  // "Product.Reviewmanagement":"Review management",
+  "Product.Reviewoverview":"Review overview shows the summary of review data in the past 180 days  ",
+  "Product.Ratingrule":"Rating rule",
+  "Product.ReviewOverallrating":"Overview：Overall rating   -    Shopping rating   -    Purchase experience rating   -    Logistic rating   -",
+  "Product.Reviewlist":"Review list",
+  "Product.Ordernumber":"Order number",
+  // "Product.Consumername":"Consumer name",
+  "Product.Productrating":"Product rating",
+  "Product.Anonymousstatus":"Anonymous status",
+  "Product.Reviewtime":"Review time",
+  "Product.Startdate":"Start date",
+  "Product.Enddate":"End date",
+  "Product.regular-product-shop":"regular-product-shop",
+  "Product.Replycomments":"Reply comments",
+  "Product.Enteruptocharacters":"Enter up to 500 characters",
 
 
   "Product.Productcategorydesc": "Product category is set by RC staff, you can associate attribute to product category.",

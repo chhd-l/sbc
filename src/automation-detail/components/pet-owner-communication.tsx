71eb20b559a81d06b197835de8f29ea59d254d06
YangSheng <kensyang@deloitte.com.cn>
2021-03-08 15:29:05 +0800

    automation

--- src/automation-detail/components/pet-owner-communication.tsx
+++ src/automation-detail/components/pet-owner-communication.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BreadCrumb, Headline, Const, AuthWrapper, history } from 'qmkit';
 import { Link } from 'react-router-dom';
-import { Table, Tooltip, Button, Form, Input, Row, Col, message, Select, Spin, Popconfirm, Switch, Breadcrumb, Card, Avatar, Pagination, Icon } from 'antd';
+import { Table, Tooltip, Button, Form, Input, Row, Col, message, Select, Spin, Popconfirm, Switch, Breadcrumb, Card, Avatar, Pagination, Icon, Modal } from 'antd';
 
 import * as webapi from './../webapi';
 import { FormattedMessage } from 'react-intl';
@@ -15,7 +15,15 @@ class PetOwnerCommunication extends Component<any, any> {
         current: 1,
         pageSize: 10,
         total: 0
-      }
+      },
+      openModalTitle: 'Pet owner communication detail',
+      petOwnerCommunicationDetailList: [],
+      modalPagination: {
+        current: 1,
+        pageSize: 10,
+        total: 0
+      },
+      visible: false
     };
   }
   componentDidMount() {}
@@ -29,9 +37,36 @@ class PetOwnerCommunication extends Component<any, any> {
     );
   };
   getpetOwnerCommunicationList = () => {};
+  getpetOwnerCommunicationDetailList = () => {};
+  handleModalTableChange = (pagination) => {
+    this.setState(
+      {
+        modalpPagination: pagination
+      },
+      () => {
+        this.getpetOwnerCommunicationDetailList();
+      }
+    );
+  };
+  openModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      visible: false,
+
+      modalPagination: {
+        current: 1,
+        pageSize: 10,
+        total: 0
+      }
+    });
+  };
 
   render() {
-    const { petOwnerCommunicationList, pagination } = this.state;
+    const { petOwnerCommunicationList, pagination, petOwnerCommunicationDetailList, openModalTitle, modalPagination, visible } = this.state;
 
     const petOwnerCommunicationColumns = [
       {
@@ -78,7 +113,7 @@ class PetOwnerCommunication extends Component<any, any> {
             <Tooltip placement="top" title="Detail">
               <a
                 onClick={() => {
-                  console.log('detail');
+                  this.openModal();
                 }}
                 className="iconfont iconDetails"
                 style={{ marginRight: 10 }}
@@ -89,11 +124,76 @@ class PetOwnerCommunication extends Component<any, any> {
       }
     ];
 
+    const communicationDetailColumns = [
+      {
+        title: 'Communication item',
+        dataIndex: 'nodeName',
+        width: '15%'
+      },
+
+      {
+        title: 'Templates',
+        dataIndex: 'templateName',
+        width: '15%'
+      },
+      {
+        title: 'Send time',
+        dataIndex: 'sendTime',
+        width: '15%'
+      },
+      {
+        title: 'Execution time',
+        dataIndex: 'executionTime',
+        width: '15%'
+      },
+      {
+        title: 'Delivered time',
+        dataIndex: 'deliveredTime',
+        width: '15%'
+      },
+      {
+        title: 'openedTime',
+        dataIndex: 'openedTime',
+        width: '15%'
+      }
+    ];
+
     return (
       <AuthWrapper functionName="f_automation_detail">
-        <Card title={'Pet owner communication'} bordered={false}>
+        <Card
+          title={'Pet owner communication'}
+          bordered={false}
+          extra={
+            <Tooltip placement="top" title="Refresh">
+              <a
+                onClick={() => {
+                  this.getpetOwnerCommunicationList();
+                }}
+                className="iconfont iconReset"
+                style={{ marginRight: 10 }}
+              ></a>
+            </Tooltip>
+          }
+        >
           <Table rowKey="id" columns={petOwnerCommunicationColumns} dataSource={petOwnerCommunicationList} pagination={pagination} scroll={{ x: '100%' }} onChange={this.handleTableChange} />
         </Card>
+        <Modal
+          title={openModalTitle}
+          visible={visible}
+          onCancel={() => this.handleClose()}
+          footer={[
+            <Button
+              key="back"
+              onClick={() => {
+                this.handleClose();
+              }}
+            >
+              Close
+            </Button>
+          ]}
+        >
+          <Table rowKey="id" columns={communicationDetailColumns} dataSource={petOwnerCommunicationDetailList} pagination={modalPagination} scroll={{ x: '100%' }} onChange={this.handleModalTableChange} />
+        </Modal>
       </AuthWrapper>
     );
   }

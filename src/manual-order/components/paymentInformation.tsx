b63f7dcf993ae41ab31b9eab6241503f4735e954
CHARLE CAI <ccharsle@gmail.com>
2021-03-12 16:23:05 +0800

    修改

--- src/manual-order/components/paymentInformation.tsx
+++ src/manual-order/components/paymentInformation.tsx
@@ -1,8 +1,16 @@
+import { cache } from 'qmkit';
 import React from 'react';
 
 export default class PaymentInformation extends React.Component<any, any> {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    //const {token}=JSON.parse(sessionStorage.getItem(cache.LOGIN_DATA)||'{}');
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInppcCI6IkRFRiJ9.eNp0jk8LgkAUxL_LO0v4lnV39VREhw4V9Ocs6-6TBHVFVynE794WRKfmOPMbZmYYxgIyMIhrS7WrvKeVcc3KtBBBpT1kKDBJMBGSR2DGwbuG-r0NHVkGkZJKsZSkKphOhLBpzFBwLJmCH78xxo2t_zv05Y66oQBttb8_EUPQ1dqXrm_e5u1yPR125-BOuh4p19aSzQfqp8rQEIh5CRk9us9nhSmPJV9eAAAA__8.DSFy91v9VnqvIKeOgmG5T1DWHqnnbiIKneqVr9iUogk';
+    window.open(`http://shopstg.royalcanin.com/de/cart?token=${token}`, 'newwindow', 'height=500, width=800, top=100, left=100, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no');
   }
 
   render() {
@@ -15,7 +23,7 @@ export default class PaymentInformation extends React.Component<any, any> {
           {stepName}
           <span className="ant-form-item-required"></span>
         </h4>
-        <div className="interaction"></div>
+        <div className="interaction">{/* <iframe src="http://shopstg.royalcanin.com/us/cart" style={{height:450,width:'100%'}} frameBorder="0"></iframe> */}</div>
       </div>
     );
   }

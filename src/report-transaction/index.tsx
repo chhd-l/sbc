091b461423bf4952fced4cd19c485e53300a2f3f
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-03-15 22:48:58 +0800

    解决analysis中的日期在safari中无法正常显示的问题

--- src/report-transaction/index.tsx
+++ src/report-transaction/index.tsx
@@ -222,8 +222,8 @@ export default class TransactionReport extends Component<any, any> {
   }
 
   onChangeDate = (date, dateString) => {
-    let startDate = moment(dateString[0]).format('YYYY-MM-DD');
-    let endDate = moment(dateString[1]).format('YYYY-MM-DD');
+    let startDate = dateString[0];
+    let endDate = dateString[1];
     this.setState(
       {
         startDate,
@@ -236,8 +236,8 @@ export default class TransactionReport extends Component<any, any> {
     );
   };
   getDefaultDate = () => {
-    let startDate = moment(new Date(this.dateCalculate(7)).toLocaleDateString()).format('YYYY-MM-DD');
-    let endDate = moment(new Date(this.dateCalculate(0)).toLocaleDateString()).format('YYYY-MM-DD');
+    let startDate = moment(sessionStorage.getItem('defaultLocalDateTime'), 'YYYY-MM-DD').subtract(7, 'days').format('YYYY-MM-DD');
+    let endDate = moment(sessionStorage.getItem('defaultLocalDateTime'), 'YYYY-MM-DD').format('YYYY-MM-DD');
     this.setState(
       {
         startDate,
@@ -478,7 +478,12 @@ export default class TransactionReport extends Component<any, any> {
               title={<p style={styles.blodFont}> {title}</p>}
               extra={
                 <div>
-                  <RangePicker onChange={this.onChangeDate} disabledDate={this.disabledDate} defaultValue={[moment(new Date(this.dateCalculate(7)), 'YYYY-MM-DD'), moment(new Date(sessionStorage.getItem('defaultLocalDateTime')), 'YYYY-MM-DD')]} format={'YYYY-MM-DD'} />
+                  <RangePicker
+                    onChange={this.onChangeDate}
+                    disabledDate={this.disabledDate}
+                    defaultValue={[moment(sessionStorage.getItem('defaultLocalDateTime'), 'YYYY-MM-DD').subtract(7, 'days'), moment(sessionStorage.getItem('defaultLocalDateTime'), 'YYYY-MM-DD')]}
+                    format={'YYYY-MM-DD'}
+                  />
                 </div>
               }
             />

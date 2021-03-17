0c1c2ce4a8291e4a770ccb18e6605cc552162460
YXYFuture <1247676987@qq.com>
2021-03-17 15:46:32 +0800

    coupon

--- src/coupon-add/store.ts
+++ src/coupon-add/store.ts
@@ -362,7 +362,7 @@ export default class AppStore extends Store {
     if (res.code == Const.SUCCESS_CODE) {
       this.dispatch('goodsActor: allGroups', res.context.segmentList);
     } else {
-      message.error('load group error.');
+      // message.error('load group error.');
     }
   };
 }

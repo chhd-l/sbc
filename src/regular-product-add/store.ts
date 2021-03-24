85d45942e9e6a9f060dd1d6e9146770fd9177ef3
drtan <1>
2021-03-24 12:00:02 +0800

    20210322

--- src/regular-product-add/store.ts
+++ src/regular-product-add/store.ts
@@ -1315,7 +1315,7 @@ export default class AppStore extends Store {
           packSize: item.get('packSize') || '',
           goodsMeasureUnit: item.get('goodsMeasureUnit') || '',
           subscriptionPrice: item.get('subscriptionPrice') || 0,
-          addedFlag: item.get('addedFlag') || 0,
+          addedFlag: item.get('addedFlag'),
           subscriptionStatus: item.get('subscriptionStatus') != undefined ? (goods.get('subscriptionStatus') == 0 ? 0 : item.get('subscriptionStatus')) : goods.get('subscriptionStatus') == 0 ? 0 : 1,
           description: item.get('description'),
           basePriceType: data.get('baseSpecId') ? data.get('baseSpecId') : '',

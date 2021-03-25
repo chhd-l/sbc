73aaf82c45e5fbd2c8399f1a283f31d5ce8ea08d
CHARLE CAI <ccharsle@gmail.com>
2021-03-25 15:20:05 +0800

    修改编辑器

--- src/regular-product-add/component/detail.tsx
+++ src/regular-product-add/component/detail.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Relax } from 'plume2';
-import { Tabs } from 'antd';
+import { Select, Tabs } from 'antd';
 import { IList, IMap } from 'typings/globalType';
 import { noop, ErrorBoundary, ReactEditor, history } from 'qmkit';
 import { List } from 'immutable';
@@ -83,14 +83,23 @@ export default class Detail extends React.Component<any, any> {
     // No state update necessary
     return null;
   }
-
+  changeTabsBar = (key,item) => {
+    let id=item.descriptionName + '_' + item.descriptionId;
+    item['contentType']=key;
+    if (key.toUpperCase() === 'TEXT') {
+      item.content = this.functionTurnJson(item.content);
+    }
+    item.key=+new Date();
+    goodsDetailTabObj[id]=item;
+    this.sortDetailTab();
+  }
   render() {
     const { goods, goodsDescriptionDetailList } = this.props.relaxProps;
     goodsDetailTabObj = {};
     return (
       <div>
         {goodsDescriptionDetailList.length > 0 && (
-          <Tabs defaultActiveKey={'main' + goodsDescriptionDetailList[0].descriptionId} animated={false}>
+          <Tabs defaultActiveKey={'main' + goodsDescriptionDetailList[0].descriptionId} animated={false} >
             {goodsDescriptionDetailList.map((item, i) => {
               goodsDetailTabObj[item.descriptionName + '_' + item.descriptionId] = item;
               let resource = goods.get('resource'),
@@ -103,9 +112,15 @@ export default class Detail extends React.Component<any, any> {
                 item.content = this.functionTurnJson(item.content);
                 item.content = `<pre type="${item.contentType.toUpperCase()}"><code><xmp>${item.content || '{tip:"Please enter the JSON format"}'}</xmp></code></pre>`;
               }
-
+              // debugger
               return (
                 <Tabs.TabPane tab={item.descriptionName} key={'main' + item.descriptionId} forceRender>
+
+                  <Select key={item.descriptionId} disabled={item?.created??true} value={item.contentType} style={{ width: 200 }} onChange={(e)=>{this.changeTabsBar(e,item)}}>
+                    <Select.Option value="text">text</Select.Option>
+                    <Select.Option value="json">json</Select.Option>
+                  </Select>
+
                   <ReactEditor
                     key={item.key}
                     id={'main-' + item.descriptionId}

c482c0d910f379fc04db5ee2c178b9a3141eb681
chuali <chuali@deioitte.com.cn>
2021-03-25 16:29:34 +0800

    first discount bug

--- src/order-detail/components/order-detail.tsx
+++ src/order-detail/components/order-detail.tsx
@@ -527,12 +527,12 @@ export default class OrderDetailTab extends React.Component<any, any> {
                 </label>
               )}
 
-             {tradePrice.firstOrderOnThePlatformDiscountPrice && (
+             {tradePrice.firstOrderOnThePlatformDiscountPrice ?  (
                 <label style={styles.priceItem as any}>
                   <span style={styles.name}>First Order Discount:</span>
                   <strong>-${tradePrice.firstOrderOnThePlatformDiscountPrice.toFixed(2)}</strong>
                 </label>
-              )}
+              ) : null}
 
               {tradePrice.promotionDiscountPrice ? (
                 <label style={styles.priceItem as any}>

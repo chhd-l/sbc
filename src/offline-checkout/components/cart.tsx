b81021d3577cb1dc880282b2e1d370a484149514
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-03-26 18:31:42 +0800

    offline checkout

--- src/offline-checkout/components/cart.tsx
+++ src/offline-checkout/components/cart.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Input, Icon } from 'antd';
 
 export default class Cart extends React.Component<any, any> {
   constructor(props: any) {
@@ -18,7 +18,18 @@ export default class Cart extends React.Component<any, any> {
           <Col span={4}>Total price</Col>
           <Col span={4}>Operation</Col>
         </Row>
-        <div className="c-cart-container"></div>
+        <div className="c-cart-container">
+          <Row className="c-cart-item">
+            <Col span={4}><img src="https://wanmi-b2b.oss-cn-shanghai.aliyuncs.com/202008030949305851.png" alt=""/></Col>
+            <Col span={4}>Puppy LATA</Col>
+            <Col span={4}>€32</Col>
+            <Col span={4}>
+              <Input suffix="kg" type="number" addonAfter={<Icon type="plus-circle" />} addonBefore={<Icon type="minus-circle" />} />
+            </Col>
+            <Col span={4}>€54</Col>
+            <Col span={4}><i className="iconfont iconDelete"></i></Col>
+          </Row>
+        </div>
         <div className="c-box-footer" style={{textAlign: 'right', lineHeight: '40px', fontSize: 16, fontWeight: 'bold'}}>
           <span style={{marginRight: 20}}>No. of products: 4</span>
           <span>total weight: 10kg</span>

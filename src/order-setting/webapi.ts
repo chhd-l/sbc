2e7208cb5b210aeddd0cf874ab9a65def186fc8b
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-03-30 10:50:21 +0800

    order setting

--- src/order-setting/webapi.ts
+++ src/order-setting/webapi.ts
@@ -19,13 +19,13 @@ export function querySysDictionary(filterParams = {}) {
   });
 }
 
-export function getOrderConfig(filterParams = {}) {
+export function getOrderSettingConfig(filterParams = {}) {
   return Fetch<TResult>('/orderConfig/query', {
     method: 'GET'
   });
 }
 
-export function updateOrderConfig(filterParams = {}) {
+export function updateOrderSettingConfig(filterParams = {}) {
   return Fetch<TResult>('/orderConfig', {
     method: 'PUT',
     body: JSON.stringify({

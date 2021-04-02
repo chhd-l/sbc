d27ee3c22485295f9b794cd13b045e99f17f387c
chuali <chuali@deioitte.com.cn>
2021-04-02 18:49:52 +0800

    max delivery

--- src/employee-list/store.ts
+++ src/employee-list/store.ts
@@ -308,10 +308,10 @@ export default class AppStore extends Store {
 
     if (this.state().get('edit')) {
       //如果非主账号，部门ID,需要拼接
-      if (this.state().get('isMaster') == 0) {
-        let restDepartmentIds = this.state().get('restDepartmentIds');
-        employeeForm.departmentIdList = employeeForm.departmentIdList.concat(restDepartmentIds);
-      }
+      // if (this.state().get('isMaster') == 0) {
+      //   let restDepartmentIds = this.state().get('restDepartmentIds');
+      //   employeeForm.departmentIdList = employeeForm.departmentIdList.concat(restDepartmentIds);
+      // }
       employeeForm.employeeId = this.state().getIn(['employeeForm', 'employeeId']);
       const { res } = await webapi.updateEmployee(employeeForm);
       //取消编辑状态

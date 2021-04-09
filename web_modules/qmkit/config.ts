321debf8c2f71fba5520dcbeeb6840dcadaa9c84
chuali <chuali@deioitte.com.cn>
2021-04-09 14:18:10 +0800

    OKTA EN Config

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,15 +1,14 @@
 import Common from './config-common';
 export default {
-  /* dev */
-  HOST: 'http://124.71.151.9:8390',
-
-  /* stg */
-  //HOST: 'https://storestg.royalcanin.com/api',
-
-  /* product */
-  //HOST: 'https://eurostore.royalcanin.com/api',
-
-
+  HOST: 'https://eurostore.royalcanin.com/api',
   CDN_PATH: '/',
-  ...Common
+  //CDN_PATH: 'https://d2cde.azureedge.net/rs/',
+  ...Common,
+  // PROD  Okta Config
+  REACT_APP_PRESCRIBER_CLIENT_ID: "0oa6ac06a7I03dDyY416",
+  REACT_APP_PRESCRIBER_ISSUER : "https://signin.royalcanin.com/oauth2/default",
+  REACT_APP_PRESCRIBER_RedirectURL: window.origin +  "/implicit/callback?type=prescriber",
+  REACT_APP_RC_CLIENT_ID: "0oa78y2vww7kzTbiq357",
+  REACT_APP_RC_ISSUER : "https://mars-group.okta.com",
+  REACT_APP_RC_RedirectURL: window.origin + "/implicit/callback?type=staff"
 };

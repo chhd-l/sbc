d50d7d4c1d23e9bd5dd10af06e72e91fe7d58787
Zhu <xingzhu@deloitte.com.cn>
2021-04-13 18:16:20 +0800

    finance 页面翻译

--- web_modules/qmkit/es_ES.ts
+++ web_modules/qmkit/es_ES.ts
@@ -2434,6 +2434,10 @@ export default {
   'Finance.saveSettings':'Save Settings',
   'Finance.add':'add',
   /* -------- 已翻译 V1.0---------------*/
+  'Finance.UnSettlement details':'UnSettlement details',
+  'Finance.Settlement details':'Settlement details',
+  "Finance.Guest": "Guest",
+  "Finance.Member": "Member",
 
 
 

9a745ecb5f850f0ec0122cc52626f3fb3d5dc218
chuali <chuali@deioitte.com.cn>
2021-04-13 20:09:10 +0800

    okta config

--- web_modules/qmkit/config-stg.ts
+++ web_modules/qmkit/config-stg.ts
@@ -6,7 +6,7 @@ export default {
   // STG  Okta Config
   REACT_APP_PRESCRIBER_CLIENT_ID: "0oaq5jv1f653OBJn80x6",
   REACT_APP_PRESCRIBER_ISSUER : "https://accountpreview.royalcanin.com/oauth2/default",
-  REACT_APP_PRESCRIBER_RedirectURL: window.origin +  "/implicit/callback",
+  REACT_APP_PRESCRIBER_RedirectURL: window.origin +  "/implicit/callback?type=prescriber",
   REACT_APP_RC_CLIENT_ID: "0oa6fb12ahvn5lAAL357",
   REACT_APP_RC_ISSUER : "https://mars-group.okta.com",
   REACT_APP_RC_RedirectURL: window.origin + "/implicit/callback?type=staff"

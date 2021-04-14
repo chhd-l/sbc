abbba9334a5aa74f67d324ad062c3ccbfaa58f34
tj <472835875@qq.com>
2021-04-14 23:29:20 +0800

    20210414

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,15 +1,17 @@
 import Common from './config-common';
 export default {
   /* dev */
-  //HOST: 'http://124.71.151.9:8390',
+  HOST: 'http://124.71.151.9:8390',
 
   /* stg */
-  HOST: 'https://storestg.royalcanin.com/api',
+  //HOST: 'https://storestg.royalcanin.com/api',
 
   /* product */
   //HOST: 'https://eurostore.royalcanin.com/api',
 
 
   CDN_PATH: '/',
-  ...Common
+  ...Common,
+  VALET_ORDER_URL:'https://shopuat.466920.com/',
+  VALET_ORDER_NOMAL_URL:'https://shopstg.royalcanin.com/'
 };

f97b437b327b0f7d16791577de26e3af3ba17240
CHARLE CAI <ccharsle@gmail.com>
2021-04-19 10:55:18 +0800

    修改样式

--- src/recommendation-add/components/fillinPetInfo.tsx
+++ src/recommendation-add/components/fillinPetInfo.tsx
@@ -193,18 +193,18 @@ export default class FillinPetInfo extends Component {
                                             disabled={petsList.length > 0 || funType}
                                         >
                                             {
-                                                petsBreedList.map(((item, index) => (<Option key={item.id}  value={item.valueEn} label={item.valueEn}>{item.name}</Option>)))
+                                                petsBreedList.map(((item, index) => (<Option key={item.id}  value={item.valueEn} label={item.valueEn}>{item.valueEn}</Option>)))
                                             }
                                         </Select>)}
                                     </Form.Item>
 
                                 </Col>
                                 <Col span={12}>
-                                    <Form.Item label="Sensitvities:">
-                                        {getFieldDecorator('sensitivity', {
-                                            initialValue: customerPet.sensitivity || '',
+                                    <Form.Item label="Special needs:">
+                                        {getFieldDecorator('needs', {
+                                            initialValue: customerPet.needs || '',
                                             //  rules: [{ required: true, message: 'Please select Sensitvities!' }],
-                                            onChange: (e,) => this._onChange(e, 'sensitivity')
+                                            onChange: (e,) => this._onChange(e, 'needs')
 
                                         })(<Select
                                             disabled={petsList.length > 0 || funType}

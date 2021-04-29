afc9fa8b006c4a8f0ab10e3bc2c8dace8b798602
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-04-29 16:44:57 +0800

    统一打包命令，不区分环境，本地开发除外

---
+++ config/env-config/prod.js
@@ -0,0 +1,17 @@
+/**
+ * @desc
+ *
+ * @使用场景
+ *
+ * @company qianmi.com
+ * @Date    2019/5/8
+ **/
+
+ var Common = require('./common');
+
+ module.exports = {
+   HOST: '/api',
+   CDN_PATH: '/',
+   ...Common
+ };
+ 

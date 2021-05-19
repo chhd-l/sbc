660b8bd43283328002411b726470dba02e596fc0
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-05-19 15:31:10 +0800

    修改subscription completed的shipmentdate字段

--- src/subscription-detail/index.tsx
+++ src/subscription-detail/index.tsx
@@ -715,7 +715,7 @@ class SubscriptionDetail extends React.Component<any, any> {
         key: 'shipmentDate',
         dataIndex: 'shipmentDate',
         width: '10%',
-        render: (text, record) => <div>{record.tradeItems[0] && record.tradeItems[0].nextDeliveryTime ? moment(record.tradeItems[0].nextDeliveryTime).format('YYYY-MM-DD') : '-'}</div>
+        render: (text, record) => <div>{record.tradeState && record.tradeState.createTime ? moment(record.tradeState.createTime).format('YYYY-MM-DD') : '-'}</div>
       },
       {
         title: (

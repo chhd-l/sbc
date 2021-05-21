5b4af6b0fb1819fa8d2e4e444fb34469105f2010
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-05-21 09:59:51 +0800

    修改subscription complete表头文字

--- src/subscription-edit/index.tsx
+++ src/subscription-edit/index.tsx
@@ -1229,7 +1229,7 @@ export default class SubscriptionDetail extends React.Component<any, any> {
         render: (text, record) => <div>{record.tradePrice && record.tradePrice.totalPrice ? currencySymbol + ' ' + record.tradePrice.totalPrice : '-'}</div>
       },
       {
-        title: <span style={{ color: '#8E8E8E', fontWeight: 500 }}><FormattedMessage id="Subscription.ShipmentDate"/></span>,
+        title: <span style={{ color: '#8E8E8E', fontWeight: 500 }}><FormattedMessage id="Order.OrderTime"/></span>,
         key: 'shipmentDate',
         dataIndex: 'shipmentDate',
         width: '10%',

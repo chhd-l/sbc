c832669acea55c4c267686c215406710b60f1696
drtan <472835875@qq.com>
2021-06-03 16:10:20 +0800

    Prescriber

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,7 +1,7 @@
 import Common from './config-common';
 export default {
   /* dev */
-  // HOST: 'https://storesit.royalcanin.com/api',
+  //HOST: 'http://124.71.151.9:8390',
 
   /* stg */
   HOST: 'https://storestg.royalcanin.com/api',

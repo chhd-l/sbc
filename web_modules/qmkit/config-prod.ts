f9a338de6c61a534581139abb26d8da62bd8e61b
Nathan <charlecai@deloitte.com.cn>
2021-07-12 18:48:36 +0800

    x修改配置

--- web_modules/qmkit/config-prod.ts
+++ web_modules/qmkit/config-prod.ts
@@ -8,9 +8,8 @@ const OKTA_APP_CONFIG = {
   REACT_APP_RC_ISSUER : "https://mars-group.okta.com",
   REACT_APP_RC_RedirectURL: window.origin + "/implicit/callback?type=staff"
 };
-
+const host = window.location.host;
 const getOktaAppConfig = () => {
-  const host = window.location.host;
   if (host === 'eurostore.royalcanin.com') {
     return Object.assign({}, OKTA_APP_CONFIG, {
       REACT_APP_PRESCRIBER_CLIENT_ID: "0oa6ac06a7I03dDyY416",
@@ -26,7 +25,71 @@ const getOktaAppConfig = () => {
   } else {
     return OKTA_APP_CONFIG;
   }
+  
 };
+const _config={
+  'storesit.royalcanin.com':{
+    PAYMENT: {
+      "ru": {
+        "app_id": "com.razorfish.sit_ms_ru",
+        "key": "0f7c56c2-e372-4aa4-8649-d89f4fb467cd",
+      },
+      "tr": {
+        "app_id": "com.razorfish.sit_iyzico_tr",
+        "key": "706da6b5-949a-44e9-95ae-d42b24382f71"
+      },
+      'de':{
+        app_id:'ROYALCAIN_GERMANY_D2C',
+        key:'pub.v2.8015632026961356.aHR0cHM6Ly9zdG9yZXNpdC5yb3lhbGNhbmluLmNvbQ.2dKUkAYcc6N_ZLnqIXeWPSxt14cFPMs0X_LJM0IBA2Q'
+      },
+      'fr':{
+        app_id:'ROYALCAIN_GERMANY_D2C',
+        key:'pub.v2.8015632026961356.aHR0cHM6Ly9zdG9yZXNpdC5yb3lhbGNhbmluLmNvbQ.2dKUkAYcc6N_ZLnqIXeWPSxt14cFPMs0X_LJM0IBA2Q'
+      }
+    }
+  },
+  'storestg.royalcanin.com':{
+    PAYMENT: {
+      "ru": {
+        "app_id": "com.razorfish.stg_ms_ru",
+        "key": "362c93e4-9865-4255-bffb-e888468e9dd2",
+      },
+      "tr": {
+        "app_id": "com.razorfish.stg_iyzico_tr",
+        "key": "7c340ecb-962b-4660-b663-78b66f790797"
+      },
+      'de':{
+        app_id:'ROYALCANIN_GERMANY_D2C',
+        key:'pub.v2.8015632026961356.aHR0cHM6Ly9zdG9yZXN0Zy5yb3lhbGNhbmluLmNvbQ._SI921RC_7nOpBTbi513hsM38WXLvY55InUJ_KkBHg8'
+      },
+      'fr':{
+        app_id:'ROYALCANIN_FRANCE_D2C',
+        key:'pub.v2.8015632026961356.aHR0cHM6Ly9zdG9yZXN0Zy5yb3lhbGNhbmluLmNvbQ._SI921RC_7nOpBTbi513hsM38WXLvY55InUJ_KkBHg8'
+      }
+    }
+  },
+  'storeuat.royalcanin.com':{
+    PAYMENT: {
+      "ru": {
+        "app_id": "com.razorfish.uat_ru_ms",
+        "key": "d594bb10-fb1f-49f2-9a82-8edea0407ceb",
+      },
+      "tr": {
+        "app_id": "com.razorfish.uat_iyzico",
+        "key": "066b7c52-daba-4aeb-8c4a-9074d85f2adc"
+      },
+      'de':{
+        app_id:'MarsIncorporated_ROYALCANIN_GERMANY_D2C_UAT_TEST',
+        key:'pub.v2.8015632026961356.aHR0cHM6Ly9zdG9yZXVhdC5yb3lhbGNhbmluLmNvbQ._gQyfk5nAPLML12ddHCBO7H3IkcPaIxxoHYahGniDJ4'
+      },
+      'fr':{
+        app_id:'MarsIncorporated_ROYALCANIN_FRANCE_D2C_UAT_TEST',
+        key:'pub.v2.8015632026961356.aHR0cHM6Ly9zdG9yZXVhdC5yb3lhbGNhbmluLmNvbQ._gQyfk5nAPLML12ddHCBO7H3IkcPaIxxoHYahGniDJ4'
+      }
+    }
+  }
+};
+
 
 
 export default {
@@ -34,5 +97,6 @@ export default {
   CDN_PATH: '/',
   ...Common,
   // Okta Config
+  ..._config[host],
   ...getOktaAppConfig()
 };

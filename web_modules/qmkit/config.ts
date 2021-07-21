a5a3d44040617132a791fb25a957f0b3772705c9
jinglv <lvjing520cxc>
2021-07-21 16:17:35 +0800

    Interface 提交

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,10 +1,10 @@
 import Common from './config-common';
 export default {
   /* dev */
-  // HOST: 'http://124.71.151.9:8390',
+  HOST: 'http://124.71.151.9:8390',
 
   /* sit */
-  HOST: 'https://storesit.royalcanin.com/api',
+  // HOST: 'https://storesit.royalcanin.com/api',
   PAYMENT_ENVIRONMENT:'test',
   /* product */
   //HOST: 'https://eurostore.royalcanin.com/api',

b99a4cff510265d32955b921f878864d594d3d9e
chunge <zhshi@deloitte.com.cn>
2021-07-27 17:36:33 +0800

    style: ref ModuleChart style

--- src/Integration/Dashboard/component/ModuleChart/index.tsx
+++ src/Integration/Dashboard/component/ModuleChart/index.tsx
@@ -5,6 +5,17 @@ export default class ModuleChart extends Component<any, any>{
     render() {
         return (
             <div className='ModuleChart-wrap'>
+                <div className='ModuleChart-main'>
+                    <div className='ModuleChart-left'>
+
+                    </div>
+                    <div className='ModuleChart-center'>
+
+                    </div>
+                    <div className='ModuleChart-right'>
+
+                    </div>
+                </div>
 
             </div>
         );

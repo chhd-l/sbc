32a6eec12ecf7d18cf90c47fe792ce5577374862
lamar <kensyang@deloitte.com.cn>
2021-07-30 17:55:43 +0800

    提交

--- src/Integration/Log/log-list/components/Logtab.tsx
+++ src/Integration/Log/log-list/components/Logtab.tsx
@@ -4,6 +4,7 @@ import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 import Tab from '@/Integration/components/tab';
 import MyTooltip from '@/Integration/components/myTooltip';
+import * as webapi from '../webapi'
 
 
 
@@ -16,8 +17,18 @@ export default class LogTabs extends Component<any, any>{
       activeTableKey: '1',
       pagination: {
         current: 1,
-        pageSize: 4,
+        pageNum:0,
+        pageSize: 10,
         total: 0
+      },
+      searchForm:{
+        prescriberId: '',
+        prescriberName: '',
+        phone: '',
+        primaryCity: '',
+        primaryZip: '',
+        prescriberType: '',
+        prescriberCode: ''
       },
       list: [
         {
@@ -488,42 +499,40 @@ export default class LogTabs extends Component<any, any>{
       columns: [
         {
           title: <FormattedMessage id="Log.RequestID" />,
-          dataIndex: 'requestid',
-          key: 'requestid'
+          dataIndex: 'prescriberId',
         },
         {
           title: <FormattedMessage id="Log.Time" />,
-          dataIndex: 'time',
+          dataIndex: 'prescriberName',
         },
         {
           title: <FormattedMessage id="Log.InterfaceName" />,
-          dataIndex: 'interfacename',
+          dataIndex: 'phone',
         },
         {
           title: <FormattedMessage id="Log.Header" />,
-          dataIndex: 'header',
+          dataIndex: 'primaryCity',
           render:(text,record) =>(
             <MyTooltip content={record.headerTip} text={text} />
           )
         },
         {
           title: <FormattedMessage id="Log.Payload" />,
-          dataIndex: 'payload',
+          dataIndex: 'primaryZip',
           render: (text, record) => (
             <MyTooltip content={record.payloadTip} text={text}/>
           )
         },
         {
           title: <FormattedMessage id="Log.Response" />,
-          dataIndex: 'response',
+          dataIndex: 'prescriberType',
           render: (text, record) => (
             <MyTooltip content={record.responseTip} text={text}/>
           )
         },
         {
           title: <FormattedMessage id="Log.ClientName" />,
-          dataIndex: 'clientname',
-          key: 'clientname',
+          dataIndex: 'prescriberCode',
         },
         {
           title: '',
@@ -538,118 +547,42 @@ export default class LogTabs extends Component<any, any>{
         }
       ],
     }
+    this.getlist(0);
+  }
+
+  initLog = async () => {
+    const { res } = await webapi.fetchLogList({
+      ...this.state.searchForm,
+      ...this.state.pagination
+    });
+    let newPagination = Object.assign({},this.state.pagination);
+    let currentLogList = res.context.content||[];
+      newPagination.total = res.context.total||0;
+      this.setState({
+        pagination:newPagination,
+        list:currentLogList
+      })
+  };
+
+  handleTableChange= (pagination: any)=> {
+    this.setState({
+      pagination: pagination
+    },()=>{
+      this.getlist(0)
+    });
   }
 
   // 切换表格获取数据
   onTableChange = (key) => {
-    this.initPage()
     this.setState({
       activeTableKey: key,
+    },()=>{
+      this.getlist(1)
     });
-    if (key == '1') {
-      this.getAllLog();
-    } else {
-      this.getError();
-    };
-
   }
 
   //获取Alllog表格数据
   getAllLog = () => {
-    const data = [
-      {
-        id: 1,
-        requestid: 1,
-        time: '2021-05-18 10:35:54.293',
-        intername: 'Price Synchronization',
-        header: 'Header',
-        headerTip:{},
-        payload:'JSON',
-        response:'R',
-        payloadTip: {
-
-            'header':{
-  
-              'x-request-id':'dc0306ca7ac6582b0ca8560bcdda115a',
-  
-              'content-length':'176',
-  
-              'country':'RU',
-  
-              'clientid':'IceROxHgyg0riyVq',
-  
-              'x-forwarded-proto':'https,http',
-  
-              'clientsecret':'1lehSUJ8i65rSfY5vSFXjPsqpQB9BJ9X',
-  
-              'x-forwarded-port':'443,443',
-  
-              'x-correlation-id':'d232f500-b7c4-11eb-a8fe-0a0b7caf7557',
-  
-              'x-forwarded-for':'10.240.2.11,10.240.3.18',
-  
-              'forwarded':'proto=http;host="open.royalcanin.com:443";for="10.240.3.18:50552"',
-  
-              'accept':'*/*',
-  
-              'x-real-ip':'10.240.2.11',
-  
-              'x-forwarded-host':'open.royalcanin.com:443,open.royalcanin.com:443',
-  
-              'host':'10.240.2.21:8690',
-  
-              'content-type':'application/json; charset=UTF-8; skipnullon="everywhere"',
-  
-              'x-scheme':'https',
-  
-              'user-agent':'AHC/1.0'
-            }
-          },
-        responseTip: {
-
-          'header':{
-
-            'x-request-id':'dc0306ca7ac6582b0ca8560bcdda115a',
-
-            'content-length':'176',
-
-            'country':'RU',
-
-            'clientid':'IceROxHgyg0riyVq',
-
-            'x-forwarded-proto':'https,http',
-
-            'clientsecret':'1lehSUJ8i65rSfY5vSFXjPsqpQB9BJ9X',
-
-            'x-forwarded-port':'443,443',
-
-            'x-correlation-id':'d232f500-b7c4-11eb-a8fe-0a0b7caf7557',
-
-            'x-forwarded-for':'10.240.2.11,10.240.3.18',
-
-            'forwarded':'proto=http;host="open.royalcanin.com:443";for="10.240.3.18:50552"',
-
-            'accept':'*/*',
-
-            'x-real-ip':'10.240.2.11',
-
-            'x-forwarded-host':'open.royalcanin.com:443,open.royalcanin.com:443',
-
-            'host':'10.240.2.21:8690',
-
-            'content-type':'application/json; charset=UTF-8; skipnullon="everywhere"',
-
-            'x-scheme':'https',
-
-            'user-agent':'AHC/1.0'
-          }
-        },
-        clientname: 'MuleSoft',
-      },
-    ];
-    this.setState({
-      list: JSON.parse(JSON.stringify(data))
-    })
   }
 
   // 获取Error表格数据
@@ -768,24 +701,48 @@ export default class LogTabs extends Component<any, any>{
 
   // 初始化分页
   initPage = () => {
+    let obj = {
+      current: 1,
+      pageNum:0,
+      pageSize: 10
+    }
     this.setState({
-      pagination: {
-        current: 1,
-        pageSize: 10
-      }
+      pagination: Object.assign(this.state.pagination,obj)
     });
+  
   };
 
+  //分页跳转
   onSearchPage = (pagination) => {
-    this.setState({
-      pagination: pagination
-    })
+    this.getlist(1)
   }
 
+
+  getlist = (val)=>{
+    if(val===1){
+      this.initPage()
+    }
+    if(this.state.activeTableKey==='1'){
+        this.initLog()
+    } else{
+        this.initError()
+    }
+  }
+  initError = () =>{
+      const objx = {
+        total:0,
+      }
+      this.setState({
+        pagination:Object.assign(this.state.pagination,objx),
+      })
+      this.setState({
+        list:[],
+      })
+  }
   render() {
     return (
       <div>
-        <Tabs defaultActiveKey={this.state.activeTableKey} onChange={(key) => this.onTableChange(key)} >
+        <Tabs activeKey={this.state.activeTableKey} onChange={(key) => this.onTableChange(key)} >
           <TabPane tab={<FormattedMessage id="Log.AllLog" />} key="1" />
           <TabPane tab={<FormattedMessage id="Log.Error" />} key="2" />
         </Tabs>
@@ -793,7 +750,7 @@ export default class LogTabs extends Component<any, any>{
           rowKey={({ id }) => id}
           dataSource={this.state.list}
           pagination={this.state.pagination}
-          onChange={this.onSearchPage}
+          onChange={this.handleTableChange}
           columns={this.state.columns}
         />
       </div>

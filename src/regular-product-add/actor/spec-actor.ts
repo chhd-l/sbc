cadb2712f23f31a1c9bdb7f59cba524bcc08d01a
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-08-24 15:25:26 +0800

    修改全局配置skuNo最大100

--- src/regular-product-add/actor/spec-actor.ts
+++ src/regular-product-add/actor/spec-actor.ts
@@ -2,7 +2,7 @@ import { Actor, Action } from 'plume2';
 import { IList, IMap } from 'typings/globalType';
 import { fromJS, Map, List } from 'immutable';
 import { message } from 'antd';
-import { cache, Const } from 'qmkit';
+import { cache, Const, RCi18n } from 'qmkit';
 
 export default class GoodsSpecActor extends Actor {
   defaultState() {
@@ -163,7 +163,7 @@ export default class GoodsSpecActor extends Actor {
 
     if (goods.count() > Const.spuMaxSku) {
       // 只进行提示，但是不拦截，保存时拦截
-      message.error(`SKU数量不超过${Const.spuMaxSku}个`);
+      message.error(RCi18n({id:'Product.Supportupto20specifications'}));
     }
 
     return state.set('goodsSpecs', goodsSpecs).set('goodsList', goods);
@@ -262,7 +262,7 @@ export default class GoodsSpecActor extends Actor {
     const goods = this._getGoods(goodsSpecs, state.get('goodsList'));
     if (goods.count() > Const.spuMaxSku) {
       // 只进行提示，但是不拦截，保存时拦截
-      message.error(`SKU数量不超过${Const.spuMaxSku}个`);
+      message.error(RCi18n({id:'Product.Supportupto20specifications'}));
     }
 
     state = state.set('goodsList', goods);

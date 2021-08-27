fc4e92307f4813f0556464a417b3cc69a99b572f
uanY <kaikpdy9@gmail.com>
2021-08-27 17:49:14 +0800

    upperloadDing

--- src/message-template-configuration/templateEdit.tsx
+++ src/message-template-configuration/templateEdit.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BreadCrumb, Headline, Const, history, RCi18n } from 'qmkit';
-import { Breadcrumb, Col, DatePicker, Form, Icon, Input, Radio, Row, Select, Spin, Tag, Tooltip } from 'antd';
+import { Breadcrumb, Button, Col, DatePicker, Form, Icon, Input, Radio, Row, Select, Spin, Tag, Tooltip } from 'antd';
 import { FormattedMessage } from '_react-intl@5.6.8@react-intl';
 import _ from 'lodash';
 
@@ -31,18 +31,73 @@ const TemplateEdit=()=>{
             <Form layout="horizontal" labelCol={{ span: 10 }} wrapperCol={{ span: 14 }} labelAlign="right">
               <Row style={{ marginTop: 20 }}>
                 <Col span={8}>
-                  <FormItem label={<FormattedMessage id="Marketing.TaskID" />}>
-                    {/*{getFieldDecorator('taskId', {*/}
-                    {/*  rules: [{ required: true }]*/}
-                    {/*})(<Input disabled />)}*/}
-                  </FormItem>
                 </Col>
               </Row>
             </Form>
           </div>
 
+          <Form layout="horizontal" labelCol={{ span: 10 }} wrapperCol={{ span: 14 }} labelAlign="right">
+            <Row style={{ marginTop: 20 }}>
+              <Col span={8}>
+                <FormItem label={<FormattedMessage id="Marketing.Email"/>}>
+                    <Input
+                      // disabled={detailForm.consumerType === 'Member' || this.state.isDetail}
+                      // // onChange={(e) => {
+                      // //   const value = (e.target as any).value;
+                      // //   this.onDetailsFormChange({
+                      // //     field: 'email',
+                      // //     value
+                      // //   });
+                      // // }}
+                    />
+                </FormItem>
+              </Col>
+              <Col span={8}>
+                <FormItem label={<FormattedMessage id="Marketing.Email"/>}>
+                  <Input
+                    // disabled={detailForm.consumerType === 'Member' || this.state.isDetail}
+                    // // onChange={(e) => {
+                    // //   const value = (e.target as any).value;
+                    // //   this.onDetailsFormChange({
+                    // //     field: 'email',
+                    // //     value
+                    // //   });
+                    // // }}
+                  />
+                </FormItem>
+              </Col>
+              <Col span={8}>
+                <FormItem label={<FormattedMessage id="Marketing.Email"/>}>
+                  <Input
+                    // disabled={detailForm.consumerType === 'Member' || this.state.isDetail}
+                    // // onChange={(e) => {
+                    // //   const value = (e.target as any).value;
+                    // //   this.onDetailsFormChange({
+                    // //     field: 'email',
+                    // //     value
+                    // //   });
+                    // // }}
+                  />
+                </FormItem>
+              </Col>
+            </Row>
+          </Form>
         </div>
       </Spin>
+
+
+
+      <div className="bar-button">
+          <Button type="primary" style={{ marginRight: 10 }}>
+            {<FormattedMessage id="Marketing.Submit" />}
+          </Button>
+          <Button type="primary" style={{ marginRight: 10 }} >
+            {<FormattedMessage id="save" />}
+          </Button>
+        <Button onClick={() => (history as any).go(-1)} style={{ marginRight: 10 }}>
+          {<FormattedMessage id="back" />}
+        </Button>
+      </div>
     </div>
   )
 }

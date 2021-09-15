901ab62db2769d4c47c31ebc9ea87ff9eda5146b
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-09-15 15:47:49 +0800

    uom category

---
+++ src/uom-category/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Input } from 'antd';
+import { useAntdTable } from 'ahooks';
+import { getUOMCategoryList } from './webapi';
+
+function UomCategory(props: any) {
+  return (
+    <div></div>
+  );
+}
+
+export default Form.create()(UomCategory);
---
+++ src/uom-category/webapi.ts
@@ -0,0 +1,33 @@
+import { Fetch, Const } from 'qmkit';
+
+type TResult = {
+  code: string;
+  message: string;
+  context: any;
+};
+
+export function getUOMCategoryList(params: any = {}) {
+  return Promise.resolve({
+    res: {
+      code: Const.SUCCESS_CODE,
+      message: '',
+      context: [
+        {
+          id: 1,
+          name: 'Unit',
+          description: 'description of unit',
+        },
+        {
+          id: 2,
+          name: 'Weight',
+          description: 'description of weight',
+        },
+        {
+          id: 3,
+          name: 'Length',
+          description: 'description of length',
+        }
+      ]
+    }
+  });
+}

d30fa1526d229b3f6789450837cd5a30c1657459
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-09-16 16:58:28 +0800

    invoice switch setting is always true and cannot be turn off

--- src/invoice-list/components/invoice-setting.tsx
+++ src/invoice-list/components/invoice-setting.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Input, Switch, Button, Spin, message } from 'antd';
 import { Const, Headline, history, RCi18n } from 'qmkit';
-import { getInvoiceConfig, setInvoiceConfig, getInvoiceSysConfig, saveInvoiceSysConfig } from '../webapi';
+import { getInvoiceSysConfig, saveInvoiceSysConfig } from '../webapi';
 import { FormattedMessage } from 'react-intl';
 import moment from 'moment';
 
@@ -17,22 +17,16 @@ const DEFAULT_FORMAT = {
 
 export default function InvoiceSetting() {
   const [loading, setLoading] = useState(true);
-  const [companyInfoId, setCompanyInfoId] = useState(0);
-  const [invoiceProjectSwitchId, setInvoiceProjectSwitchId] = useState('');
-  const [isSupportInvoice, setIsSupportInvoice] = useState(0);
   const [invoicePrefix, setInvoicePrefix] = useState(DEFAULT_PREFIX);
   const [invoiceFormat, setInvoiceFormat] = useState(DEFAULT_FORMAT);
 
   const getInvoiceSetting = () => {
-    Promise.all([getInvoiceConfig(), getInvoiceSysConfig('myvet_invoice_prefix'), getInvoiceSysConfig('myvet_invoice_format')]).then(([data1, data2, data3]) => {
-      if (data1.res.code === Const.SUCCESS_CODE && data2.res.code === Const.SUCCESS_CODE && data3.res.code === Const.SUCCESS_CODE) {
+    Promise.all([getInvoiceSysConfig('myvet_invoice_prefix'), getInvoiceSysConfig('myvet_invoice_format')]).then(([data1, data2]) => {
+      if (data1.res.code === Const.SUCCESS_CODE && data2.res.code === Const.SUCCESS_CODE) {
         setLoading(false);
-        setCompanyInfoId(data1.res.context?.companyInfoId ?? 0);
-        setInvoiceProjectSwitchId(data1.res.context?.invoiceProjectSwitchId ?? '');
-        setIsSupportInvoice(data1.res.context?.isSupportInvoice ?? 0);
 
-        setInvoicePrefix(data2.res.context ? data2.res.context[0] : DEFAULT_PREFIX);
-        setInvoiceFormat(data3.res.context ? data3.res.context[0] : DEFAULT_FORMAT);
+        setInvoicePrefix(data1.res.context ? data1.res.context[0] : DEFAULT_PREFIX);
+        setInvoiceFormat(data2.res.context ? data2.res.context[0] : DEFAULT_FORMAT);
       }
     });
   };
@@ -42,23 +36,14 @@ export default function InvoiceSetting() {
   }, []);
 
   const submitInvoiceSetting = () => {
-    if (isSupportInvoice === 1 && !invoiceFormat.context.trim()) {
+    if (!invoiceFormat.context.trim()) {
       message.error(RCi18n({id:'Finance.InvoiceFormatMustFill'}));
       return;
     }
     setLoading(true);
-    Promise.all([
-      setInvoiceConfig({
-        companyInfoId,
-        invoiceProjectSwitchId,
-        isPaperInvoice: isSupportInvoice,
-        isSupportInvoice,
-        isValueAddedTaxInvoice: isSupportInvoice
-      }),
-      saveInvoiceSysConfig([invoicePrefix, invoiceFormat])
-    ]).then(([data1, data2]) => {
-      if (data1.res.code === Const.SUCCESS_CODE && data2.res.code === Const.SUCCESS_CODE) {
-        message.success(data1.res.message);
+    saveInvoiceSysConfig([invoicePrefix, invoiceFormat]).then((data) => {
+      if (data.res.code === Const.SUCCESS_CODE) {
+        message.success(data.res.message);
         setLoading(false);
       }
     });
@@ -80,27 +65,23 @@ export default function InvoiceSetting() {
         <Form layout="horizontal" labelCol={{span: 4}} wrapperCol={{span:12}}>
           <FormItem label={<FormattedMessage id="Finance.SupportInvoice" />}>
             <Switch
-              checked={isSupportInvoice === 1}
-              onChange={(checked) => {
-                setIsSupportInvoice(checked ? 1 : 0);
-              }}
+              checked={true}
+              disabled
             />
           </FormItem>
-          {isSupportInvoice === 1 && <>
-            <FormItem label={<FormattedMessage id="Finance.InvoicePrefix" />}>
-              <Input value={invoicePrefix.context} onChange={(e) => setInvoicePrefix(Object.assign({}, invoicePrefix, {context:e.target.value}))} />
-            </FormItem>
-            <FormItem label={<FormattedMessage id="Finance.InvoiceFormat" />} required help={<FormattedMessage id="Finance.InvoiceTips" />}>
-              <Input value={invoiceFormat.context} onChange={(e) => setInvoiceFormat(Object.assign({}, invoiceFormat, {context:e.target.value}))} />
-            </FormItem>
-            <FormItem label={<FormattedMessage id="Finance.InvoiceNumberExample" />}>
-              <Input.TextArea
-                autoSize 
-                readOnly 
-                value={genExampleInvoiceNumber('1') + '\n' + genExampleInvoiceNumber('2') + '\n' + genExampleInvoiceNumber('3')}
-              />
-            </FormItem>
-          </>}
+          <FormItem label={<FormattedMessage id="Finance.InvoicePrefix" />}>
+            <Input value={invoicePrefix.context} onChange={(e) => setInvoicePrefix(Object.assign({}, invoicePrefix, {context:e.target.value}))} />
+          </FormItem>
+          <FormItem label={<FormattedMessage id="Finance.InvoiceFormat" />} required help={<FormattedMessage id="Finance.InvoiceTips" />}>
+            <Input value={invoiceFormat.context} onChange={(e) => setInvoiceFormat(Object.assign({}, invoiceFormat, {context:e.target.value}))} />
+          </FormItem>
+          <FormItem label={<FormattedMessage id="Finance.InvoiceNumberExample" />}>
+            <Input.TextArea
+              autoSize 
+              readOnly 
+              value={genExampleInvoiceNumber('1') + '\n' + genExampleInvoiceNumber('2') + '\n' + genExampleInvoiceNumber('3')}
+            />
+          </FormItem>
           <FormItem wrapperCol={{span:16,offset:4}}>
             <Button type="primary" onClick={submitInvoiceSetting}><FormattedMessage id="Finance.saveSettings" /></Button>
             <Button style={{marginLeft: 10}} onClick={() => history.go(-1)}><FormattedMessage id="Finance.back" /></Button>

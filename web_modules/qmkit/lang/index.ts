df44072e2fbc94758e3ee6b15abeaf1cad002420
fuluo <fuluo@deloitte.com.cn>
2021-09-22 18:58:39 +0800

    动态加载 phrase language

--- web_modules/qmkit/lang/index.ts
+++ web_modules/qmkit/lang/index.ts
@@ -1,27 +1,68 @@
 
 import { cache } from 'qmkit';
-const context = (require as any).context('./files',true, /\.ts$/)
-const importAll = context => {
-    const map = {}
-    for (const key of context.keys()) {
-      const keyArr = key.split('/')
-      keyArr.shift() // 移除.
-      map[keyArr.join('.').replace(/\.ts$/g, '')] =context(key).default|| context(key)
-    }
-    return map
-  }
 
-let key = sessionStorage.getItem(cache.LANGUAGE)||'en-US'
-let langFile = importAll(context)
+const context = (require as any).context('./files', true, /\.ts$/);
+const importAll = context => {
+  const map = {};
+  for (const key of context.keys()) {
+    const keyArr = key.split('/');
+    keyArr.shift(); // 移除.
+    map[keyArr.join('.').replace(/\.ts$/g, '')] = context(key).default || context(key);
+  }
+  return map;
+};
+
+let key = sessionStorage.getItem(cache.LANGUAGE) || 'en-US';
+let langFile = importAll(context);
 let language: any = langFile[key];
 let antLanguage: any = langFile[key + '_antd'];
 
-function RCi18n ({id}) {
-    return language[id] || id;
+
+function RCi18n({ id }) {
+  return language[id] || id;
 }
+
+
+const PHRASE_PROJECT_ID = 'b16c5e4ed825ad40972b1d8379cb733e';
+
+async function getDynamicLanguage() {
+  const url = `https://api.phrase.com/v2/projects/${PHRASE_PROJECT_ID}/locales/${key}/download?access_token=31950e3e49b165b8b2c604b65574e6cf279d9ea395e3718ce52b1ec335bef6e5&include_empty_translations=true&file_format=node_json`;
+
+  const res = await fetch(url, {
+    method: 'get',
+    mode: 'cors',
+    cache: 'no-store',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }).then(res => {
+
+    return res.ok ? res.json() : false;
+
+  }).then(resJson => {
+
+    let retRes: {};
+
+    if (resJson) {
+      retRes = resJson;
+      window.localStorage.setItem('PHRASE_LANGUAGE', JSON.stringify(retRes));
+    } else {
+      retRes = JSON.parse(window.localStorage.getItem('PHRASE_LANGUAGE')) || {};
+    }
+
+    return retRes;
+  });
+
+  language = { ...langFile[key], ...res };
+
+  return language;
+}
+
 
 export {
   language,
   antLanguage,
-  RCi18n
-}
+  RCi18n,
+  getDynamicLanguage
+};

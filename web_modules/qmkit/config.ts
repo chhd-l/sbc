e6e407cdcf6977d8faafa5666fb8dbe967f51991
chunge <1362598656@qq.com>
2021-09-26 11:27:55 +0800

    ref: 新增sit的环境api地址

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -11,10 +11,13 @@ const COLORS = {
 
 export default {
   /* dev */
-  //HOST: 'http://121.36.199.41:8390',
+  HOST: 'http://121.36.199.41:8390',
+
+  /* sit */
+  // HOST: 'https://storesit.royalcanin.com/api',
 
   /* stg */
-  HOST: 'https://storestg.royalcanin.com/api',
+  //HOST: 'https://storestg.royalcanin.com/api',
   PAYMENT_ENVIRONMENT:'test',
   /* product */
   //HOST: 'https://eurostore.royalcanin.com/api',

7fee5516ba3c3de718a1ee8bea5699570e02838f
zuoqin <qzuo@deloitte.com.cn>
2021-10-19 15:54:13 +0800

    order detail 页面优化 增加注释

---
+++ src/order-detail/components/type.ts
@@ -0,0 +1,53 @@
+import { IMap } from 'plume2';
+
+export interface Consignee {
+  detailAddress: string;
+  name: string;
+  phone: string;
+  countryId: string;
+  city: string;
+  province: string;
+  cityId: number;
+  address: string;
+  detailAddress1: string;
+  detailAddress2: string;
+  rfc: string;
+  postCode: string;
+  firstName: string;
+  lastName: string;
+  comment: string;
+  entrance: string;
+  apartment: string;
+  area: string;
+  timeSlot: string;
+  deliveryDate: string;
+  workTime: string;
+}
+
+export interface Invoice{
+  open: boolean; //是否需要开发票
+  type: number; //发票类型
+  title: string; //发票抬头
+  projectName: string; //开票项目名称
+  generalInvoice: IMap; //普通发票
+  specialInvoice: IMap; //增值税专用发票
+  address: string;
+  address1: string;
+  address2: string;
+  contacts: string; //联系人
+  phone: string; //联系方式
+  provinceId: number;
+  cityId: number;
+  province: string;
+  countryId: number;
+  // city:string;
+  // province:string;
+  firstName: string;
+  lastName: string;
+  postCode: string;
+  city: string;
+  comment: string;
+  entrance: string;
+  apartment: string;
+  area: string;
+}

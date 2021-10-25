6f66e3d17e28e3430e5020436fbd6208fbdff5cb
zuoqin <qzuo@deloitte.com.cn>
2021-10-25 17:47:10 +0800

    update

--- src/order-detail/components/order-detail.tsx
+++ src/order-detail/components/order-detail.tsx
@@ -205,7 +205,7 @@ class OrderDetailTab extends React.Component<any, any> {
         key: 'skuName',
         width: '9%',
         render: (text, record) => {
-          const productName = text === 'individualization' ? record.petsName + "'s" + text : text;
+          const productName = text === 'individualization' ? record.petsName + '\'s' + text : text;
           return (
             <Tooltip
               overlayStyle={{

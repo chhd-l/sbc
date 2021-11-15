52f956e4e4529a262d95d7315c43af64af75202f
Yi, Timyee Dianbo <tiyi@deloitte.com.cn>
2021-11-15 16:25:33 +0800

    fix owner and controller of myvetreco open store type

--- src/basic-setting/myvetreco/repre.tsx
+++ src/basic-setting/myvetreco/repre.tsx
@@ -78,11 +78,11 @@ class ShareHolder extends React.Component<RepreFormProps, any> {
           <Col span={12}>
             <FormItem label="Shareholder type" required>
               {getFieldDecorator('shareholderType', {
-                initialValue: "1"
+                initialValue: "0"
               })(
               <Select onChange={this.validateJobTitle} disabled={adyenAuditState === 0}>
-                <Option value="1">Owner</Option>
-                <Option value="2">Controller</Option>
+                <Option value="0">Owner</Option>
+                <Option value="1">Controller</Option>
               </Select>
               )}
             </FormItem>
@@ -131,7 +131,7 @@ class ShareHolder extends React.Component<RepreFormProps, any> {
           <Col span={24}>
             <FormItem label="Job title" labelCol={{span: 4}} wrapperCol={{span: 12}} extra={<div style={{color:'red'}}>Only needed if shareholder type is controller</div>}>
               {getFieldDecorator('jobTitle', {
-                rules: [{ required: form.getFieldValue('shareholderType') === "2", message: 'Please input job title' }]
+                rules: [{ required: form.getFieldValue('shareholderType') === "1", message: 'Please input job title' }]
               })(
                 <Input disabled={adyenAuditState === 0} />
               )}

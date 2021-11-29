efdec2a38b3d46d745e91b69731e2d9602aba1fa
fuluo <fuluo@deloitte.com.cn>
2021-11-29 14:23:43 +0800

    邮件预览

--- src/message-email-list/messageTemplateDetail.tsx
+++ src/message-email-list/messageTemplateDetail.tsx
@@ -1,89 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import {  Modal, Button, Form, Input, Row, Col, message, Select, Spin } from 'antd';
-import * as webapi from './webapi'
+import { Modal, Button, Form, Input, Row, Col, message, Select, Spin } from 'antd';
+import * as webapi from './webapi';
 import { Const } from 'qmkit';
-import Handlebars from 'handlebars'
-import SendSay from '../../web_modules/qmkit/sendsay';
-
-Handlebars.registerHelper('equals', function(arg1, arg2, options) {
-  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-});
-Handlebars.registerHelper('length', function(arg1,options) {
-  return arg1.length;
-});
-Handlebars.registerHelper('greaterThan', function(arg1, arg2, options) {
-  return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
-});
-Handlebars.registerHelper('length', function(fn) {
-  return ('');
-});
+import { getPreviewEmailTemp } from '../../web_modules/qmkit/previewEmail';
 
 
+const MessageTemplateDetail = ({ visibleTemplate, setVisibleTemplate, taskId }) => {
 
-const MessageTemplateDetail=({visibleTemplate,setVisibleTemplate,taskId})=>{
-
-    const [loading,setLoading]=useState(true)
-    const [previewHtml,setPreviewHtml]=useState('')
-    const [templateTaskId,setTemplateTaskId]=useState('')
-
-
-    const params={
-        taskId:taskId
-      }
-
-    const getEmailTask=()=>{
-        setLoading(true)
-        webapi
-          .viewEmailTask(params)
-          .then((data)=>{
-              const {res} =data;
-              if(res.code === Const.SUCCESS_CODE){
-                setLoading(false);
-                const templateDate =res.context;
-                const emailData = JSON.parse(templateDate.messageSendParams).templateData
-                const emailTemp = templateDate.messageTemplateContent
-
-                let template = null;
-
-                if (false) {
-                  template = new SendSay(emailTemp, {anketa: {params: {...emailData}}}).getTemplate()
-                } else {
-                  template = Handlebars.compile(emailTemp)(emailData)
-                }
-
-                setTemplateTaskId(templateDate.messageTaskId)
-                setPreviewHtml(template)
+  const [loading, setLoading] = useState(true);
+  const [previewHtml, setPreviewHtml] = useState('');
+  const [templateTaskId, setTemplateTaskId] = useState('');
 
 
-              }
-          })
+  const params = {
+    taskId: taskId
+  };
+
+  const getEmailTask = () => {
+    setLoading(true);
+    webapi
+      .viewEmailTask(params)
+      .then((data) => {
+        const { res } = data;
+        if (res.code === Const.SUCCESS_CODE) {
+          setLoading(false);
+
+          const templateDate = res.context;
+          const emailTemp = templateDate.messageTemplateContent;
+          const emailData = JSON.parse(templateDate.messageSendParams).templateData;
+
+          const template = getPreviewEmailTemp(emailTemp, emailData);
+
+          setTemplateTaskId(templateDate.messageTaskId);
+          setPreviewHtml(template);
+        }
+      });
+  };
+
+  useEffect(() => {
+    if (visibleTemplate) {
+      getEmailTask();
     }
+  }, [visibleTemplate]);
 
-    useEffect(()=>{
-      if(visibleTemplate){
-        getEmailTask()
-      }
-    },[visibleTemplate])
+  return (
+    <>
+      <Modal
+        width='800px'
+        title={templateTaskId}
+        onCancel={() => setVisibleTemplate()}
+        visible={visibleTemplate}
+        footer={[
+          <Button key='back' shape='round' onClick={() => setVisibleTemplate()}>
+            Cancel
+          </Button>
+        ]}
+      >
+        <Spin spinning={loading}>
+          {previewHtml ? <div dangerouslySetInnerHTML={{ __html: previewHtml }} style={{ zoom: '0.5' }}></div> : null}
+        </Spin>
+      </Modal>
+    </>
+  );
+};
 
-    return(
-      <>
-        <Modal
-            width="800px"
-            title={templateTaskId}
-            onCancel={()=>setVisibleTemplate()}
-            visible={visibleTemplate}
-            footer={[
-                <Button key="back" shape="round" onClick={()=>setVisibleTemplate()}>
-                    Cancel
-                </Button>
-            ]}
-        >
-          <Spin spinning={loading}>
-            {previewHtml?<div dangerouslySetInnerHTML={{__html:previewHtml}} style={{zoom:'0.5'}}></div>:null}
-          </Spin>
-        </Modal>
-      </>
-    )
-}
-
-export default MessageTemplateDetail
+export default MessageTemplateDetail;

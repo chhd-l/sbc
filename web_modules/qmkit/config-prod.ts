f3d9a554a989fa9cd3ce9b6e7f215132ee4a3b64
梦醒家 <hongxiang.qu@effem.com>
2022-07-14 19:54:11 +0800

    update REACT_APP_RC_CLIENT_ID

--- web_modules/qmkit/config-prod.ts
+++ web_modules/qmkit/config-prod.ts
@@ -5,11 +5,13 @@ const OKTA_APP_CONFIG = {
   REACT_APP_PRESCRIBER_CLIENT_ID: '0oaq5jv1f653OBJn80x6',
   REACT_APP_PRESCRIBER_ISSUER: 'https://accountpreview.royalcanin.com/oauth2/default',
   REACT_APP_PRESCRIBER_RedirectURL: window.origin + '/implicit/callback?type=prescriber',
-  REACT_APP_RC_CLIENT_ID: '0oa6fb12ahvn5lAAL357',
+  //REACT_APP_RC_CLIENT_ID: '0oa6fb12ahvn5lAAL357',
+  REACT_APP_RC_CLIENT_ID: '0oajhi9s4alu99qIn357',
   REACT_APP_RC_ISSUER: 'https://mars-group.okta.com',
   REACT_APP_RC_RedirectURL: window.origin + '/implicit/callback?type=staff'
 };
 const host = window.location.host;
+console.log({host})
 const getOktaAppConfig = () => {
   if (host === 'eurostore.royalcanin.com') {
     return Object.assign({}, OKTA_APP_CONFIG, {

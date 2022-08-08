5d7b7b75894654824755062cbd77501325ebaf40
mingyi.tang@effem.com <mingyi.tang@effem.com>
2022-08-08 14:42:55 +0800

    425950 [CORE] SEO settings for Navigation List

--- web_modules/qmkit/config.ts
+++ web_modules/qmkit/config.ts
@@ -1,4 +1,11 @@
-
+/*
+ * @Author: mingyi.tang@effem.com mingyi.tang@effem.com
+ * @Date: 2022-08-08 10:24:13
+ * @LastEditors: mingyi.tang@effem.com mingyi.tang@effem.com
+ * @LastEditTime: 2022-08-08 14:35:31
+ * @FilePath: \sbc-supplier-front\web_modules\qmkit\config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Common from './config-common';
 
 const host = window.location.host;

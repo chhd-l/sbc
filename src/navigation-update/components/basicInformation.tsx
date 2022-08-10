f91d3a51a3261b5d98a67e62616856363705b6dd
mingyi.tang@effem.com <mingyi.tang@effem.com>
2022-08-10 16:06:35 +0800

    425950 [CORE] SEO settings for Navigation List

--- src/navigation-update/components/basicInformation.tsx
+++ src/navigation-update/components/basicInformation.tsx
@@ -82,16 +82,21 @@ export default class BasicInformation extends React.Component<any, any> {
                 rules: [
                   {
                     validator: (rule, value, callback) => {
-                      webapi.getSeoNavigation(value).then((res) => {
-                        let { id } = res.res.context.seoSettingVO;
-                        if (id) {
-                          callback(<FormattedMessage id="Content.NavigationLinkTips" />);
-                        } else if (testlink.test(value)) {
-                          callback();
-                        } else {
-                          callback(<FormattedMessage id="Content.NavigationLinkRoute" />);
-                        }
-                      });
+                      if (testlink.test(value)) {
+                        callback();
+                      } else {
+                        callback(<FormattedMessage id="Content.NavigationLinkRoute" />);
+                      }
+                      // webapi.getSeoNavigation(value).then((res) => {
+                      //   let { id } = res.res.context.seoSettingVO;
+                      //   if (id) {
+                      //     callback(<FormattedMessage id="Content.NavigationLinkTips" />);
+                      //   } else if (testlink.test(value)) {
+                      //     callback();
+                      //   } else {
+                      //     callback(<FormattedMessage id="Content.NavigationLinkRoute" />);
+                      //   }
+                      // });
                     }
                   }
                 ]

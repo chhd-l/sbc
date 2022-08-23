136c01ae23dfa6c1841102095e59ddd3f4fc7783
mingyi.tang@effem.com <mingyi.tang@effem.com>
2022-08-23 09:01:10 +0800

    435474: Ability to add a prescriber to a PO

---
+++ src/customer-details/component/prescriber.tsx
@@ -0,0 +1,269 @@
+import { Button, Col, Form, Icon, Input, message, Modal, Row, Table } from 'antd';
+import React, { useState } from 'react';
+import { useCallbackState } from 'use-callback-state';
+import { Const, RCi18n } from 'qmkit';
+import { FormattedMessage } from 'react-intl';
+import * as webapi from './webapi';
+const FormItem = Form.Item;
+const styles = {
+  label: {
+    width: 120,
+    textAlign: 'center'
+  }
+} as any;
+const columns_attribute = [
+  {
+    title: <FormattedMessage id="Product.AttributeName" />,
+    dataIndex: 'attributeName',
+    key: 'attributeName'
+  },
+  {
+    title: <FormattedMessage id="Product.DisplayName" />,
+    dataIndex: 'attributeNameEn',
+    key: 'attributeNameEn'
+  },
+  {
+    title: <FormattedMessage id="Product.AttributeValue" />,
+    dataIndex: 'attributeValue',
+    key: 'attributeValue',
+    width: '30%',
+    render: (text, record) => (
+      <p>
+        {record.attributesValuesVOList ? this.getAttributeValue(record.attributesValuesVOList) : ''}
+      </p>
+    )
+  }
+];
+const prescriber = () => {
+  const [visible, setVisible] = useState(false);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [oldSelectedRowKeys, setOldSelectedRowKeys] = useState([]);
+  const [prevPropSelectedRowKeys, setPrevPropSelectedRowKeys] = useState([]);
+  const [attributeList, setAttributeList] = useState([]);
+  const [selectedRowList, setSelectedRowList] = useState([]);
+  const [oldSelectedRowList, setOldSelectedRowList] = useState([]);
+  const [prevPropSelectedRowList, setPrevPropSelectedRowList] = useState([]);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [pagination, setPagination]: any = useCallbackState(
+    {
+      current: 1,
+      pageSize: 8,
+      total: 0
+    },
+    () => {}
+  );
+  const [searchForm, setSearchForm] = useState({
+    attributeName: '',
+    attributeValue: ''
+  });
+  const handleOk = () => {
+    setConfirmLoading(true);
+    let storeGoodsFilterList = [];
+    for (let i = 0; i < selectedRowList.length; i++) {
+      let item = {
+        attributeId: selectedRowList[i].id,
+        attributeName: selectedRowList[i].attributeName,
+        filterType: '0',
+        filterStatus: '1'
+      };
+      storeGoodsFilterList.push(item);
+    }
+    let params = {
+      storeGoodsFilterList: storeGoodsFilterList
+    };
+    webapi
+      .addAttributeToFilter(params)
+      .then((data) => {
+        const { res } = data;
+        if (res.code === Const.SUCCESS_CODE) {
+          message.success(<FormattedMessage id="Product.OperateSuccessfully" />);
+          setConfirmLoading(false);
+          setVisible(false);
+          //   this.props.refreshList();
+        } else {
+          setConfirmLoading(true);
+        }
+      })
+      .catch((err) => {
+        setConfirmLoading(false);
+      });
+  };
+  const handleCancel = () => {
+    setVisible(false);
+  };
+  const onFormChange = ({ field, value }) => {
+    let data = searchForm;
+    data[field] = value;
+    setSearchForm({ ...data });
+  };
+  const getAttributes = () => {
+    const { pagination, searchForm } = this.state;
+    let params = {
+      attributeName: searchForm.attributeName,
+      attributeValue: searchForm.attributeValue,
+      attributeStatus: true,
+      pageSize: pagination.pageSize,
+      pageNum: pagination.current - 1,
+      searchType: 'filter'
+    };
+    webapi
+      .getAttributes(params)
+      .then((data) => {
+        const { res } = data;
+        if (res.code === Const.SUCCESS_CODE) {
+          pagination.total = res.context.total;
+          const attributeList = res.context.attributesList;
+          setAttributeList({ ...pagination });
+        }
+      })
+      .catch((err) => {
+        message.error(err.toString() || <FormattedMessage id="Product.OperationFailed" />);
+      });
+  };
+  const onSearch = () => {
+    setPagination(
+      {
+        current: 1,
+        pageSize: 8,
+        total: 0
+      },
+      () => {
+        getAttributes();
+      }
+    );
+  };
+  const onSelectChange = (selectedRowKeys, selectedRow) => {
+    setSelectedRowList([...selectedRowList.concat(selectedRow)]);
+    setSelectedRowList([...arrayFilter(selectedRowKeys, selectedRowList)]);
+    setSelectedRowKeys([...selectedRowKeys]);
+  };
+  const arrayFilter = (arrKey, arrList) => {
+    let tempList = [];
+    arrKey.map((item) => {
+      tempList.push(arrList.find((el) => el.id === item));
+    });
+    return tempList;
+  };
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    getCheckboxProps: (record) => ({
+      disabled: JSON.stringify(oldSelectedRowKeys).indexOf(record.id) !== -1, // Column configuration not to be checked
+      name: record.id
+    })
+  };
+  const handleTableChange = (pagination: any) => {
+    setPagination({ ...pagination }, () => getAttributes());
+  };
+  const openSelectAttribute = () => {
+    setVisible(true);
+    setPagination(
+      {
+        current: 1,
+        pageSize: 8,
+        total: 0
+      },
+      () => {}
+    );
+    setSearchForm({
+      attributeName: '',
+      attributeValue: ''
+    });
+    getAttributes();
+  };
+  return (
+    <>
+      <Button
+        type="primary"
+        style={{ margin: '10px 0 10px 0' }}
+        onClick={() => openSelectAttribute()}
+      >
+        <span>
+          <FormattedMessage id="Finance.AddNew" />
+        </span>
+      </Button>
+      <Modal
+        title={<FormattedMessage id="Product.SelectAttribute" />}
+        visible={visible}
+        width="800px"
+        confirmLoading={confirmLoading}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <div>
+          <div style={{ marginBottom: 16 }}>
+            <Form className="filter-content" layout="inline">
+              <Row>
+                <Col span={10}>
+                  <FormItem>
+                    <Input
+                      addonBefore={
+                        <p style={styles.label} title={RCi18n({ id: 'Product.AttributeName' })}>
+                          <FormattedMessage id="Product.AttributeName" />
+                        </p>
+                      }
+                      value={searchForm.attributeName}
+                      onChange={(e) => {
+                        const value = (e.target as any).value;
+                        onFormChange({
+                          field: 'attributeName',
+                          value
+                        });
+                      }}
+                    />
+                  </FormItem>
+                </Col>
+                <Col span={10}>
+                  <FormItem>
+                    <Input
+                      addonBefore={
+                        <p style={styles.label} title={RCi18n({ id: 'Product.AttributeValue' })}>
+                          <FormattedMessage id="Product.AttributeValue" />
+                        </p>
+                      }
+                      value={searchForm.attributeValue}
+                      onChange={(e) => {
+                        const value = (e.target as any).value;
+                        onFormChange({
+                          field: 'attributeValue',
+                          value
+                        });
+                      }}
+                    />
+                  </FormItem>
+                </Col>
+                <Col span={4} style={{ textAlign: 'center' }}>
+                  <FormItem>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      icon="search"
+                      shape="round"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        onSearch();
+                      }}
+                    >
+                      <span>
+                        <FormattedMessage id="Product.search" />
+                      </span>
+                    </Button>
+                  </FormItem>
+                </Col>
+              </Row>
+            </Form>
+          </div>
+          <Table
+            rowKey="id"
+            rowSelection={rowSelection}
+            columns={columns_attribute}
+            dataSource={attributeList}
+            onChange={handleTableChange}
+            pagination={pagination}
+          />
+        </div>
+      </Modal>
+    </>
+  );
+};
+export default prescriber;
